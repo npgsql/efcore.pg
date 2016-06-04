a8999f215ab273c08c466b27661a2cd87a34c2be
Shay Rojansky <roji@roji.org>
2016-06-04 11:26:07 +0300

    Revert "Fix test parallelization"
    This reverts commit dc96fd07684abf3a4c4752598b539612875dc03b.
    
    There are still some parallelization-related issues.

--- test/Npgsql.EntityFrameworkCore.PostgreSQL.FunctionalTests/Properties/AssemblyInfo.cs
+++ test/Npgsql.EntityFrameworkCore.PostgreSQL.FunctionalTests/Properties/AssemblyInfo.cs
@@ -4,3 +4,6 @@ using Xunit;
 
 [assembly: AssemblyTitle("Npgsql.EntityFrameworkCore.PostgreSQL.FunctionalTests")]
 [assembly: AssemblyDescription("Functional test suite for Npgsql PostgreSQL provider for Entity Framework Core")]
+
+// There seem to be some issues running in parallel... See #23
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
