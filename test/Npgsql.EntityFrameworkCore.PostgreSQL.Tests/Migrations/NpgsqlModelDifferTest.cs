aa659d8d5130eaf08ad29b4e96c2855ad10cfdf9
Shay Rojansky <roji@roji.org>
2016-07-27 10:20:00 +0000

    Reorder migration operations correctly
    PostgreSQL extension creation now happens after schema creation,
    and drop now happens after all other drop operations.
    
    Fixes #76
    
    (cherry picked from commit b2720c56b6b8ca16320118fce70533390fb358cb)

---
+++ test/Npgsql.EntityFrameworkCore.PostgreSQL.Tests/Migrations/NpgsqlModelDifferTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Relational.Tests.Migrations.Internal;
+using Xunit;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL.Tests.Migrations
+{
+    public class NpgsqlModelDifferTest : MigrationsModelDifferTestBase
+    {
+        [Fact]
+        public void Create_extension_comes_after_ensure_schema()
+        {
+            Execute(
+                _ => { },
+                modelBuilder =>
+                {
+                    modelBuilder.HasDefaultSchema("some_schema");
+                    modelBuilder.HasPostgresExtension("some_extension");
+                    modelBuilder.Entity("some_table", x => x.Property<int>("Id"));
+                },
+                operations =>
+                {
+                    Assert.Equal(3, operations.Count);
+
+                    var createSchemaOperation = Assert.IsType<EnsureSchemaOperation>(operations[0]);
+                    Assert.Equal("some_schema", createSchemaOperation.Name);
+
+                    var createPostgresExtensionOperation = Assert.IsType<NpgsqlCreatePostgresExtensionOperation>(operations[1]);
+                    Assert.Equal("some_schema", createPostgresExtensionOperation.Schema);
+                    Assert.Equal("some_extension", createPostgresExtensionOperation.Name);
+                });
+        }
+    }
+}
