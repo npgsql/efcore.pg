bd5c606a4e1642b2dfc43b124bf84a6561cd641a
Shay Rojansky <roji@roji.org>
2016-09-17 21:03:18 +0000

    Sync NpgsqlTestStore with EFCore
    Also tightened some database delete/create logic
    
    (cherry picked from commit f45cfee24052ec7e2aea3c7da8d183fad00f458e)

--- src/Npgsql.EntityFrameworkCore.PostgreSQL/Migrations/NpgsqlMigrationsSqlGenerator.cs
+++ src/Npgsql.EntityFrameworkCore.PostgreSQL/Migrations/NpgsqlMigrationsSqlGenerator.cs
@@ -400,7 +400,7 @@ namespace Microsoft.EntityFrameworkCore.Migrations
                 .AppendLine(SqlGenerationHelper.StatementTerminator)
                 // TODO: For PG <= 9.1, the column name is prodpic, not pid (see http://stackoverflow.com/questions/5408156/how-to-drop-a-postgresql-database-if-there-are-active-connections-to-it)
                 .Append(
-                    "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '")
+                    "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = '")
                 .Append(operation.Name)
                 .Append("'")
                 .AppendLine(SqlGenerationHelper.StatementTerminator)
