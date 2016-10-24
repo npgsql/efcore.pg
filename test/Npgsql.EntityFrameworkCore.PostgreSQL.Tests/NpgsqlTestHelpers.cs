bec37108812bfdc02359c9a2db7e7255cdfa71ca
Shay Rojansky <roji@roji.org>
2016-10-24 20:39:33 +0000

    Removed accidental using

--- test/Npgsql.EntityFrameworkCore.PostgreSQL.Tests/NpgsqlTestHelpers.cs
+++ test/Npgsql.EntityFrameworkCore.PostgreSQL.Tests/NpgsqlTestHelpers.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Tests;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
