28212c7ea1d74e58e3339070bbb8d05e2420ff12
Shay Rojansky <roji@roji.org>
2016-11-20 12:09:29 +0200

    Bump version and pin dependencies to 1.1.0
    Also depend on Npgsql 3.1.9

--- src/Shared/CommonAssemblyInfo.cs
+++ src/Shared/CommonAssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Resources;
 // The following version attributes get rewritten by GitVersion as part of the build
 [assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyFileVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0-ci")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
