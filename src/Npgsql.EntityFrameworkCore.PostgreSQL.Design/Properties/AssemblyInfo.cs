3b592375296b4d98067c10783eca6a062e34d8c7
Shay Rojansky <roji@roji.org>
2017-02-23 07:42:47 +0200

    Migrate to new simplified csproj (VS2017)
    (cherry picked from commit 06cc5b9f443c1a4f4dda5e3e1f1a7933f574c98c)

--- src/Npgsql.EntityFrameworkCore.PostgreSQL.Design/Properties/AssemblyInfo.cs
+++ src/Npgsql.EntityFrameworkCore.PostgreSQL.Design/Properties/AssemblyInfo.cs
@@ -1,10 +1,2 @@
 ﻿using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Npgsql.EntityFrameworkCore.PostgreSQL.Design")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
--- test/Npgsql.EntityFrameworkCore.PostgreSQL.Design.FunctionalTests/Properties/AssemblyInfo.cs
+++ test/Npgsql.EntityFrameworkCore.PostgreSQL.Design.FunctionalTests/Properties/AssemblyInfo.cs
@@ -2,8 +2,5 @@ using System.Reflection;
 using System.Resources;
 using Xunit;
 
-[assembly: AssemblyTitle("Npgsql.EntityFrameworkCore.PostgreSQL.Design.FunctionalTests")]
-[assembly: AssemblyDescription("Functional test suite for Npgsql PostgreSQL design provider for Entity Framework Core")]
-
 // There seem to be some issues running in parallel... See #23
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
