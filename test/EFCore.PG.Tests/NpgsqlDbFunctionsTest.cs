165b06d9ca080ce647057f565625ca60da424b95
Shay Rojansky <roji@roji.org>
2017-11-20 23:09:50 +0300

    Fix C# impl of ILike to be case insensitive
    Fixes #267

---
+++ test/EFCore.PG.Tests/NpgsqlDbFunctionsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable InconsistentNaming
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public class NpgsqlDbFunctionsTest
+    {
+        readonly DbFunctions _functions = EF.Functions;
+
+        [Fact]
+        public void ILike_when_no_wildcards()
+        {
+            Assert.True(_functions.ILike("abc", "abc"));
+            Assert.True(_functions.ILike("abc", "ABC"));
+            Assert.True(_functions.ILike("ABC", "abc"));
+
+            Assert.False(_functions.ILike("ABC", "ab"));
+            Assert.False(_functions.ILike("ab", "abc"));
+        }
+    }
+}
