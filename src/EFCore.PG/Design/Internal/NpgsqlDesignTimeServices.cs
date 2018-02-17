23bff796449ee140b1c6d7e41cc8bc22b3107abc
Shay Rojansky <roji@roji.org>
2018-02-17 12:13:37 +0100

    Fix scaffolding bug

--- src/EFCore.PG/Design/Internal/NpgsqlDesignTimeServices.cs
+++ src/EFCore.PG/Design/Internal/NpgsqlDesignTimeServices.cs
@@ -36,7 +36,7 @@ namespace Microsoft.EntityFrameworkCore.Design.Internal
             => serviceCollection
                 .AddSingleton<IRelationalTypeMappingSource, NpgsqlTypeMappingSource>()
                 .AddSingleton<IDatabaseModelFactory, NpgsqlDatabaseModelFactory>()
-                .AddSingleton<ProviderCodeGenerator, NpgsqlCodeGenerator>()
+                .AddSingleton<IProviderCodeGenerator, NpgsqlCodeGenerator>()
                 .AddSingleton<IAnnotationCodeGenerator, NpgsqlAnnotationCodeGenerator>();
     }
 }
