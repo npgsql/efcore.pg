2b30b6c313588acdb20399ee4f2e0ad06c174362
Shay Rojansky <roji@roji.org>
2018-03-05 08:34:24 +0200

    Add missing citext mapping

---
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqCitextTypeMapping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.NetworkInformation;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Utilities;
+using NpgsqlTypes;
+
+namespace Microsoft.EntityFrameworkCore.Storage.Internal.Mapping
+{
+    public class NpgsqlCitextTypeMapping : NpgsqlTypeMapping
+    {
+        public NpgsqlCitextTypeMapping() : base("citext", typeof(string), NpgsqlDbType.Citext) {}
+
+        protected override string GenerateNonNullSqlLiteral(object value)
+            => $"CITEXT '{EscapeSqlLiteral((string)value)}'";
+
+        string EscapeSqlLiteral([NotNull] string literal)
+            => Check.NotNull(literal, nameof(literal)).Replace("'", "''");
+    }
+}
