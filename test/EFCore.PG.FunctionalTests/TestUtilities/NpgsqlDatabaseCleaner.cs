29043ef9faa42fa2992fb62014957f2d0197e984
Shay Rojansky <roji@roji.org>
2018-04-09 22:24:44 +0200

    Improve test's database cleaner
    Better handling of dropping extensions

--- test/EFCore.PG.FunctionalTests/TestUtilities/NpgsqlDatabaseCleaner.cs
+++ test/EFCore.PG.FunctionalTests/TestUtilities/NpgsqlDatabaseCleaner.cs
@@ -1,6 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Scaffolding;
@@ -31,15 +35,59 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities
         protected override bool AcceptIndex(DatabaseIndex index)
             => false;
 
-        protected override string BuildCustomEndingSql(DatabaseModel databaseModel)
+        const string GetExtensions = @"
+SELECT name FROM pg_available_extensions WHERE installed_version IS NOT NULL AND name <> 'plpgsql'";
+
+        public override void Clean(DatabaseFacade facade)
         {
+            // The following is somewhat hacky
+            // PostGIS creates some system tables (e.g. spatial_ref_sys) which can't be dropped until the extension
+            // is dropped. But our tests create some user tables which depend on PostGIS. So we clean out PostGIS
+            // and all tables that depend on it (CASCADE) before the database model is built.
+            var creator = facade.GetService<IRelationalDatabaseCreator>();
+            var connection = facade.GetService<IRelationalConnection>();
+            if (creator.Exists())
+            {
+                connection.Open();
+                try
+                {
+                    var dbConnection = (NpgsqlConnection)connection.DbConnection;
+
+                    List<string> extensions;
+                    using (var cmd = new NpgsqlCommand(GetExtensions, dbConnection))
+                    using (var reader = cmd.ExecuteReader())
+                        extensions = reader.Cast<DbDataRecord>().Select(r => r.GetString(0)).ToList();
+
+                    var dropExtensionsSql = string.Join("", extensions.Select(e => $"DROP EXTENSION \"{e}\" CASCADE;"));
+                    using (var cmd = new NpgsqlCommand(dropExtensionsSql, dbConnection))
+                        cmd.ExecuteNonQuery();
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            base.Clean(facade);
+        }
+
+        protected override string BuildCustomSql(DatabaseModel databaseModel)
+        {
+            // Some extensions create tables (e.g. PostGIS), wo we must drop them first.
             var sb = new StringBuilder();
 
             foreach (var extension in PostgresExtension.GetPostgresExtensions(databaseModel))
                 sb
                     .Append("DROP EXTENSION ")
                     .Append(_sqlGenerationHelper.DelimitIdentifier(extension.Name, extension.Schema))
-                    .AppendLine(" CASCADE;");
+                    .AppendLine(";");
+
+            return sb.ToString();
+        }
+
+        protected override string BuildCustomEndingSql(DatabaseModel databaseModel)
+        {
+            var sb = new StringBuilder();
 
             foreach (var enumDef in PostgresEnum.GetPostgresEnums(databaseModel))
                 sb
