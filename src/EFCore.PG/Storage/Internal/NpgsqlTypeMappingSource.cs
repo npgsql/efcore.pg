npgsql/efcore.pg

commit 888e0a0358afbac58e50e9cddc3d84c3fe298b73

Support mapping PG arrays to List<T>
No operation translation yet

Closes #392

src/EFCore.PG/Storage/Internal/NpgsqlTypeMappingSource.cs
@@ -26,6 +26,7 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -272,6 +273,20 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal
 
         protected override RelationalTypeMapping FindMapping(in RelationalTypeMappingInfo mappingInfo)
         {
+            var baseMapping = FindBaseTypeMapping(mappingInfo);
+            if (baseMapping != null)
+                return baseMapping;
+
+            // We couldn't find a base (simple) type mapping. Try to find an array.
+            var arrayMapping = FindArrayMapping(mappingInfo);
+            if (arrayMapping != null)
+                return arrayMapping;
+
+            return null;
+        }
+
+        protected virtual RelationalTypeMapping FindBaseTypeMapping(in RelationalTypeMappingInfo mappingInfo)
+        {
             var clrType = mappingInfo.ClrType;
             var storeTypeName = mappingInfo.StoreTypeName;
             var storeTypeNameBase = mappingInfo.StoreTypeNameBase;
@@ -302,19 +317,14 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal
                     return null;
                 }
 
-                return FindArrayMapping(mappingInfo);
+                return null;
             }
 
             if (clrType == null)
                 return null;
 
             if (!_clrTypeMappings.TryGetValue(clrType, out var mapping))
-            {
-                // TODO: range, enum, composite
-
-                // No mapping found which corresponds to the clrType, try to find an array
-                return FindArrayMapping(mappingInfo);
-            }
+                return null;
 
             // If needed, clone the mapping with the configured length/precision/scale
             // TODO: Cache size/precision/scale mappings?
@@ -352,12 +362,14 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal
             return mapping;
         }
 
-        RelationalTypeMapping FindArrayMapping(RelationalTypeMappingInfo mappingInfo)
+        RelationalTypeMapping FindArrayMapping(in RelationalTypeMappingInfo mappingInfo)
         {
             // PostgreSQL array type names are the element plus []
             var storeType = mappingInfo.StoreTypeName;
             if (storeType != null && storeType.EndsWith("[]"))
             {
+                // Note that we scaffold PostgreSQL arrays to C# arrays, not lists (which are also supported)
+
                 // TODO: In theory support the multiple mappings just like we do with scalars above
                 // (e.g. DateTimeOffset[] vs. DateTime[]
                 var elementMapping = FindMapping(storeType.Substring(0, storeType.Length - 2));
@@ -370,13 +382,13 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal
             if (clrType == null)
                 return null;
 
-            // Try to see if it is an array type
-            var arrayElementType = GetArrayElementType(clrType);
-            if (arrayElementType != null)
+            if (clrType.IsArray)
             {
-                var elementMapping = (RelationalTypeMapping)FindMapping(arrayElementType);
+                var elementType = clrType.GetElementType();
+                Debug.Assert(elementType != null, "Detected array type but element type is null");
 
                 // If an element isn't supported, neither is its array
+                var elementMapping = (RelationalTypeMapping)FindMapping(elementType);
                 if (elementMapping == null)
                     return null;
 
@@ -387,18 +399,23 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal
                 return _clrTypeMappings.GetOrAdd(clrType, new NpgsqlArrayTypeMapping(elementMapping, clrType));
             }
 
+            if (clrType.IsGenericType && clrType.GetGenericTypeDefinition() == typeof(List<>))
+            {
+                var elementType = clrType.GetGenericArguments()[0];
+
+                // If an element isn't supported, neither is its array
+                var elementMapping = (RelationalTypeMapping)FindMapping(elementType);
+                if (elementMapping == null)
+                    return null;
+
+                // Arrays of arrays aren't supported (as opposed to multidimensional arrays) by PostgreSQL
+                if (elementMapping is NpgsqlArrayTypeMapping)
+                    return null;
+
+                return _clrTypeMappings.GetOrAdd(clrType, new NpgsqlListTypeMapping(elementMapping, clrType));
+            }
+
             return null;
-        }
-
-        [CanBeNull]
-        static Type GetArrayElementType(Type type)
-        {
-            var typeInfo = type.GetTypeInfo();
-            if (typeInfo.IsArray)
-                return type.GetElementType();
-
-            var ilist = typeInfo.ImplementedInterfaces.FirstOrDefault(x => x.GetTypeInfo().IsGenericType && x.GetGenericTypeDefinition() == typeof(IList<>));
-            return ilist != null ? ilist.GetGenericArguments()[0] : null;
         }
     }
 }

src/EFCore.PG/Storage/Internal/Mapping/NpgsqlListTypeMapping.cs
@@ -32,48 +32,39 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
 {
     /// <summary>
-    /// Maps PostgreSQL arrays to .NET arrays. Only single-dimensional arrays are supported.
+    /// Maps PostgreSQL arrays to .NET List{T}.
     /// </summary>
-    /// <remarks>
-    /// Note that mapping PostgreSQL arrays to .NET List{T} is also supported via <see cref="NpgsqlListTypeMapping"/>.
-    /// </remarks>
-    public class NpgsqlArrayTypeMapping : RelationalTypeMapping
+    public class NpgsqlListTypeMapping : RelationalTypeMapping
     {
         public RelationalTypeMapping ElementMapping { get; }
 
         /// <summary>
         /// Creates the default array mapping (i.e. for the single-dimensional CLR array type)
         /// </summary>
-        public NpgsqlArrayTypeMapping(string storeType, RelationalTypeMapping elementMapping)
-            : this(storeType, elementMapping, elementMapping.ClrType.MakeArrayType())
+        public NpgsqlListTypeMapping(RelationalTypeMapping elementMapping, Type listType)
+            : this(elementMapping.StoreType + "[]", elementMapping, listType)
         {}
 
-        /// <summary>
-        /// Creates the default array mapping (i.e. for the single-dimensional CLR array type)
-        /// </summary>
-        public NpgsqlArrayTypeMapping(RelationalTypeMapping elementMapping, Type arrayType)
-            : this(elementMapping.StoreType + "[]", elementMapping, arrayType)
-        {}
-
-        NpgsqlArrayTypeMapping(string storeType, RelationalTypeMapping elementMapping, Type arrayType)
+        NpgsqlListTypeMapping(string storeType, RelationalTypeMapping elementMapping, Type listType)
             : base(new RelationalTypeMappingParameters(
-                new CoreTypeMappingParameters(arrayType, null, CreateComparer(elementMapping, arrayType)), storeType
+                new CoreTypeMappingParameters(listType, null, CreateComparer(elementMapping, listType)), storeType
             ))
         {
             ElementMapping = elementMapping;
         }
 
-        protected NpgsqlArrayTypeMapping(RelationalTypeMappingParameters parameters, RelationalTypeMapping elementMapping)
+        protected NpgsqlListTypeMapping(RelationalTypeMappingParameters parameters, RelationalTypeMapping elementMapping)
             : base(parameters) {}
 
         public override RelationalTypeMapping Clone(string storeType, int? size)
-            => new NpgsqlArrayTypeMapping(StoreType, ElementMapping);
+            => new NpgsqlListTypeMapping(StoreType, ElementMapping, ClrType);
 
         public override CoreTypeMapping Clone(ValueConverter converter)
-            => new NpgsqlArrayTypeMapping(Parameters.WithComposedConverter(converter), ElementMapping);
+            => new NpgsqlListTypeMapping(Parameters.WithComposedConverter(converter), ElementMapping);
 
         protected override string GenerateNonNullSqlLiteral(object value)
         {
+            // TODO: Duplicated from NpgsqlArrayTypeMapping
             var arr = (Array)value;
 
             if (arr.Rank != 1)
@@ -93,14 +84,14 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
 
         #region Value Comparison
 
-        static ValueComparer CreateComparer(RelationalTypeMapping elementMapping, Type arrayType)
-        {
-            Debug.Assert(arrayType.IsArray);
-            var elementType = arrayType.GetElementType();
+        // Note that the value comparison code is largely duplicated from NpgsqlAraryTypeMapping.
+        // However, a limitation in EF Core prevents us from merging the code together, see
+        // https://github.com/aspnet/EntityFrameworkCore/issues/11077
 
-            // We currently don't support mapping multi-dimensional arrays.
-            if (arrayType.GetArrayRank() != 1)
-                return null;
+        static ValueComparer CreateComparer(RelationalTypeMapping elementMapping, Type listType)
+        {
+            Debug.Assert(listType.IsGenericType && listType.GetGenericTypeDefinition() == typeof(List<>));
+            var elementType = listType.GetGenericArguments()[0];
 
             // We use different comparer implementations based on whether we have a non-null element comparer,
             // and if not, whether the element is IEquatable<TElem>
@@ -117,44 +108,44 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
             return (ValueComparer)Activator.CreateInstance(typeof(SingleDimComparerWithEquals<>).MakeGenericType(elementType));
         }
 
-        class SingleDimComparerWithComparer<TElem> : ValueComparer<TElem[]>
+        class SingleDimComparerWithComparer<TElem> : ValueComparer<List<TElem>>
         {
             public SingleDimComparerWithComparer(RelationalTypeMapping elementMapping) : base(
                 (a, b) => Compare(a, b, (ValueComparer<TElem>)elementMapping.Comparer),
                 o => o.GetHashCode(), // TODO: Need to get hash code of elements...
                 source => Snapshot(source, (ValueComparer<TElem>)elementMapping.Comparer)) {}
 
-            public override Type Type => typeof(TElem[]);
+            public override Type Type => typeof(List<TElem>);
 
-            static bool Compare(TElem[] a, TElem[] b, ValueComparer<TElem> elementComparer)
+            static bool Compare(List<TElem> a, List<TElem> b, ValueComparer<TElem> elementComparer)
             {
-                if (a.Length != b.Length)
+                if (a.Count != b.Count)
                     return false;
 
                 // Note: the following currently boxes every element access because ValueComparer isn't really
                 // generic (see https://github.com/aspnet/EntityFrameworkCore/issues/11072)
-                for (var i = 0; i < a.Length; i++)
+                for (var i = 0; i < a.Count; i++)
                     if (!elementComparer.Equals(a[i], b[i]))
                         return false;
 
                 return true;
             }
 
-            static TElem[] Snapshot(TElem[] source, ValueComparer<TElem> elementComparer)
+            static List<TElem> Snapshot(List<TElem> source, ValueComparer<TElem> elementComparer)
             {
                 if (source == null)
                     return null;
 
-                var snapshot = new TElem[source.Length];
+                var snapshot = new List<TElem>(source.Count);
                 // Note: the following currently boxes every element access because ValueComparer isn't really
                 // generic (see https://github.com/aspnet/EntityFrameworkCore/issues/11072)
-                for (var i = 0; i < source.Length; i++)
-                    snapshot[i] = elementComparer.Snapshot(source[i]);
+                foreach (var e in source)
+                    snapshot.Add(elementComparer.Snapshot(e));
                 return snapshot;
             }
         }
 
-        class SingleDimComparerWithIEquatable<TElem> : ValueComparer<TElem[]>
+        class SingleDimComparerWithIEquatable<TElem> : ValueComparer<List<TElem>>
             where TElem : IEquatable<TElem>
         {
             public SingleDimComparerWithIEquatable(): base(
@@ -162,14 +153,14 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
                 o => o.GetHashCode(), // TODO: Need to get hash code of elements...
                 source => DoSnapshot(source)) {}
 
-            public override Type Type => typeof(TElem[]);
+            public override Type Type => typeof(List<TElem>);
 
-            static bool Compare(TElem[] a, TElem[] b)
+            static bool Compare(List<TElem> a, List<TElem> b)
             {
-                if (a.Length != b.Length)
+                if (a.Count != b.Count)
                     return false;
 
-                for (var i = 0; i < a.Length; i++)
+                for (var i = 0; i < a.Count; i++)
                 {
                     var elem1 = a[i];
                     var elem2 = b[i];
@@ -187,34 +178,34 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
                 return true;
             }
 
-            static TElem[] DoSnapshot(TElem[] source)
+            static List<TElem> DoSnapshot(List<TElem> source)
             {
                 if (source == null)
                     return null;
-                var snapshot = new TElem[source.Length];
-                for (var i = 0; i < source.Length; i++)
-                    snapshot[i] = source[i];
+                var snapshot = new List<TElem>(source.Count);
+                foreach (var e in source)
+                    snapshot.Add(e);
                 return snapshot;
             }
         }
 
-        class SingleDimComparerWithEquals<TElem> : ValueComparer<TElem[]>
+        class SingleDimComparerWithEquals<TElem> : ValueComparer<List<TElem>>
         {
             public SingleDimComparerWithEquals() : base(
                 (a, b) => Compare(a, b),
                 o => o.GetHashCode(), // TODO: Need to get hash code of elements...
                 source => DoSnapshot(source)) {}
 
-            public override Type Type => typeof(TElem[]);
+            public override Type Type => typeof(List<TElem>);
 
-            static bool Compare(TElem[] a, TElem[] b)
+            static bool Compare(List<TElem> a, List<TElem> b)
             {
-                if (a.Length != b.Length)
+                if (a.Count != b.Count)
                     return false;
 
                 // Note: the following currently boxes every element access because ValueComparer isn't really
                 // generic (see https://github.com/aspnet/EntityFrameworkCore/issues/11072)
-                for (var i = 0; i < a.Length; i++)
+                for (var i = 0; i < a.Count; i++)
                 {
                     var elem1 = a[i];
                     var elem2 = b[i];
@@ -231,16 +222,16 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
                 return true;
             }
 
-            static TElem[] DoSnapshot(TElem[] source)
+            static List<TElem> DoSnapshot(List<TElem> source)
             {
                 if (source == null)
                     return null;
 
-                var snapshot = new TElem[source.Length];
+                var snapshot = new List<TElem>(source.Count);
                 // Note: the following currently boxes every element access because ValueComparer isn't really
                 // generic (see https://github.com/aspnet/EntityFrameworkCore/issues/11072)
-                for (var i = 0; i < source.Length; i++)
-                    snapshot[i] = source[i];
+                foreach (var e in source)
+                    snapshot.Add(e);
                 return snapshot;
             }
         }

test/EFCore.PG.FunctionalTests/Query/ArrayQueryTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.TestUtilities;
@@ -18,6 +19,7 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query
             {
                 var x = ctx.SomeEntities.Single(e => e.Id == 1);
                 Assert.Equal(new[] { 3, 4 }, x.SomeArray);
+                Assert.Equal(new List<int> { 3, 4 }, x.SomeList);
             }
         }
 
@@ -192,6 +194,7 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query
         public int Id { get; set; }
         public int[] SomeArray { get; set; }
         public int[,] SomeMatrix { get; set; }
+        public List<int> SomeList { get; set; }
         public byte[] SomeBytea { get; set; }
         public string SomeText { get; set; }
     }
@@ -221,14 +224,16 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query
                     Id=1,
                     SomeArray = new[] { 3, 4 },
                     SomeBytea = new byte[] { 3, 4 },
-                    SomeMatrix = new[,] { { 5, 6 }, { 7, 8 } }
+                    SomeMatrix = new[,] { { 5, 6 }, { 7, 8 } },
+                    SomeList = new List<int> { 3, 4 }
                 });
                 ctx.SomeEntities.Add(new SomeArrayEntity
                 {
                     Id=2,
                     SomeArray = new[] { 5, 6, 7 },
                     SomeBytea = new byte[] { 5, 6, 7 },
-                    SomeMatrix = new[,] { { 10, 11 }, { 12, 13 } }
+                    SomeMatrix = new[,] { { 10, 11 }, { 12, 13 } },
+                    SomeList = new List<int> { 3, 4 }
                 });
                 ctx.SaveChanges();
             }

commit 681fd2f5774e60438cd2f3cbe0a74c24dc75b3e9

Fixed array-related mapping bug
Introduced in 888e0a0358afbac58e50e9cddc3d84c3fe298b73

src/EFCore.PG/Storage/Internal/NpgsqlTypeMappingSource.cs
@@ -366,8 +366,11 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal
         {
             // PostgreSQL array type names are the element plus []
             var storeType = mappingInfo.StoreTypeName;
-            if (storeType != null && storeType.EndsWith("[]"))
+            if (storeType != null)
             {
+                if (!storeType.EndsWith("[]"))
+                    return null;
+
                 // Note that we scaffold PostgreSQL arrays to C# arrays, not lists (which are also supported)
 
                 // TODO: In theory support the multiple mappings just like we do with scalars above