a19cb78c3cf5f629953cc74363a0e39d84d3e319
Shay Rojansky <roji@roji.org>
2018-06-03 12:42:32 +0100

    Document why we run tests with extra threads

--- test/EFCore.PG.FunctionalTests/Properties/AssemblyInfo.cs
+++ test/EFCore.PG.FunctionalTests/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Reflection;
 using System.Resources;
 using Xunit;
 
+// For the following see https://github.com/aspnet/EntityFrameworkCore/issues/11848
 [assembly: CollectionBehavior(MaxParallelThreads = 4)]
