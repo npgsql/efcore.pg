74475bd16fa43bbf790f3841d8da720d63f782db
Shay Rojansky <roji@roji.org>
2018-06-18 13:58:21 +0100

    Add type mapping for lo
    Also rearrange and clean up the mappings a little.
    
    Fixes #455
    
    (cherry picked from commit 90ee95dd9984fa6adc9f19e49278471200d95f02)

---
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqlUIntTypeMapping.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using NpgsqlTypes;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
+{
+    public class NpgsqlUintTypeMapping : NpgsqlTypeMapping
+    {
+        public NpgsqlUintTypeMapping(string storeType, NpgsqlDbType npgsqlDbType)
+            : base(storeType, typeof(uint), npgsqlDbType) {}
+
+        protected NpgsqlUintTypeMapping(RelationalTypeMappingParameters parameters, NpgsqlDbType npgsqlDbType)
+            : base(parameters, npgsqlDbType) {}
+
+        public override RelationalTypeMapping Clone(string storeType, int? size)
+            => new NpgsqlUintTypeMapping(Parameters.WithStoreTypeAndSize(storeType, size), NpgsqlDbType);
+
+        public override CoreTypeMapping Clone(ValueConverter converter)
+            => new NpgsqlUintTypeMapping(Parameters.WithComposedConverter(converter), NpgsqlDbType);
+    }
+}
