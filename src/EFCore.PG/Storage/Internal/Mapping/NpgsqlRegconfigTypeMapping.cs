e356ef18db5e25c93d5f8f7ea1f1ab203ea8ea51
Shay Rojansky <roji@roji.org>
2018-07-14 12:20:43 +0100

    Add mapping for regconfig type
    Fixes #516
    
    (cherry picked from commit 76da2f335a6dafbaaa73661392d8e5eaffaf328a)

---
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqlRegconfigTypeMapping.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Utilities;
+using NpgsqlTypes;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
+{
+    public class NpgsqlRegconfigTypeMapping : NpgsqlTypeMapping
+    {
+        public NpgsqlRegconfigTypeMapping() : base("regconfig", typeof(string), NpgsqlDbType.Unknown) { }
+
+        protected NpgsqlRegconfigTypeMapping(RelationalTypeMappingParameters parameters, NpgsqlDbType npgsqlDbType)
+            : base(parameters, npgsqlDbType) {}
+
+        public override RelationalTypeMapping Clone(string storeType, int? size)
+            => new NpgsqlRegconfigTypeMapping(Parameters.WithStoreTypeAndSize(storeType, size), NpgsqlDbType);
+
+        public override CoreTypeMapping Clone(ValueConverter converter)
+            => new NpgsqlRegconfigTypeMapping(Parameters.WithComposedConverter(converter), NpgsqlDbType);
+    }
+}
