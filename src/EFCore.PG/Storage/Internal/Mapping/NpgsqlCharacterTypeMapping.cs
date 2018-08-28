dc75cfd60f94ce1e8b281b9cf65afab59b3149ab
Austin Drenski <austin.drenski@gmail.com>
2018-08-28 13:15:21 -0400

    Update with docs and base call

--- src/EFCore.PG/Storage/Internal/Mapping/NpgsqlCharacterTypeMapping.cs
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqlCharacterTypeMapping.cs
@@ -6,18 +6,24 @@ using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
 {
-    /// <inheritdoc />
     /// <summary>
-    ///
+    /// The type mapping for the PostgreSQL 'character' data type.
     /// </summary>
     /// <remarks>
     /// See: https://www.postgresql.org/docs/current/static/datatype-character.html
     /// </remarks>
+    /// <inheritdoc />
     public class NpgsqlCharacterTypeMapping : NpgsqlStringTypeMapping
     {
         /// <summary>
         /// Static <see cref="ValueComparer{T}"/> for fixed-width character types.
         /// </summary>
+        /// <remarks>
+        /// Comparisons of 'character' data as defined in the SQL standard
+        /// differ dramatically from CLR string comparisons. This value comparer
+        /// adjusts for this by only comparing strings after truncating trailing
+        /// whitespace.
+        /// </remarks>
         [NotNull] static readonly ValueComparer<string> CharacterValueComparer =
             new ValueComparer<string>(
                 (x, y) => x != null && y != null && x.AsSpan().TrimEnd() == y.AsSpan().TrimEnd(),
@@ -30,22 +36,17 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
         public NpgsqlCharacterTypeMapping([NotNull] string storeType, bool unicode = false, int? size = null)
             : base(storeType, unicode, size) {}
 
-        protected NpgsqlCharacterTypeMapping(RelationalTypeMappingParameters parameters)
-            : base(parameters) {}
+        protected NpgsqlCharacterTypeMapping(RelationalTypeMappingParameters parameters) : base(parameters) {}
 
         protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
             => new NpgsqlCharacterTypeMapping(parameters);
 
         protected override void ConfigureParameter(DbParameter parameter)
         {
-            var value = parameter.Value as string;
-
-            if (value != null)
+            if (parameter.Value is string value)
                 parameter.Value = value.TrimEnd();
 
-            // See #357
-            if (Size.HasValue)
-                parameter.Size = parameter.Value == DBNull.Value || value == null || value.Length <= Size.Value ? Size.Value : -1;
+            base.ConfigureParameter(parameter);
         }
     }
 }
--- src/EFCore.PG/Storage/Internal/Mapping/NpgsqlStringTypeMapping.cs
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqlStringTypeMapping.cs
@@ -5,28 +5,26 @@ using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
 {
-    // TODO: See #357. We should be able to simply use StringTypeMapping but DbParameter.Size isn't managed properly.
-    /// <inheritdoc />
     /// <summary>
-    ///
+    /// The type mapping for the PostgreSQL 'text' data type.
     /// </summary>
     /// <remarks>
-    ///
+    /// See: https://www.postgresql.org/docs/current/static/datatype-character.html
     /// </remarks>
+    /// <inheritdoc />
     public class NpgsqlStringTypeMapping : StringTypeMapping
     {
         public NpgsqlStringTypeMapping([NotNull] string storeType, bool unicode = false, int? size = null)
             : base(storeType, System.Data.DbType.String, unicode, size) {}
 
-        protected NpgsqlStringTypeMapping(RelationalTypeMappingParameters parameters)
-            : base(parameters) {}
+        protected NpgsqlStringTypeMapping(RelationalTypeMappingParameters parameters) : base(parameters) {}
 
         protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
             => new NpgsqlStringTypeMapping(parameters);
 
         protected override void ConfigureParameter(DbParameter parameter)
         {
-            // See #357
+            // TODO: See #357. We should be able to simply use StringTypeMapping but DbParameter.Size isn't managed properly.
             if (Size.HasValue)
             {
                 var value = parameter.Value;
