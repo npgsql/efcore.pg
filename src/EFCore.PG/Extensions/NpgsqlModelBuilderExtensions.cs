63d3e298823cf5820a36019a9c12d9e78e782c4b
Austin Drenski <austin.drenski@gmail.com>
2018-08-29 21:47:55 -0400

    Add overload to infer from CLR enum to scaffold
    - `PgNameAttribute` is respected for the type and members.

--- src/EFCore.PG/Extensions/NpgsqlModelBuilderExtensions.cs
+++ src/EFCore.PG/Extensions/NpgsqlModelBuilderExtensions.cs
@@ -1,24 +1,61 @@
-﻿using JetBrains.Annotations;
+﻿#region License
+
+// The PostgreSQL License
+//
+// Copyright (C) 2016 The Npgsql Development Team
+//
+// Permission to use, copy, modify, and distribute this software and its
+// documentation for any purpose, without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph and the following two paragraphs appear in all copies.
+//
+// IN NO EVENT SHALL THE NPGSQL DEVELOPMENT TEAM BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+// INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+// DOCUMENTATION, EVEN IF THE NPGSQL DEVELOPMENT TEAM HAS BEEN ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+// THE NPGSQL DEVELOPMENT TEAM SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS
+// ON AN "AS IS" BASIS, AND THE NPGSQL DEVELOPMENT TEAM HAS NO OBLIGATIONS
+// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+#endregion
+
+using System;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Npgsql;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Utilities;
+using Npgsql.NameTranslation;
+using NpgsqlTypes;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.EntityFrameworkCore
 {
     /// <summary>
-    ///     Npgsql specific extension methods for <see cref="ModelBuilder" />.
+    /// Npgsql specific extension methods for <see cref="ModelBuilder"/>.
     /// </summary>
+    [PublicAPI]
     public static class NpgsqlModelBuilderExtensions
     {
+        // TODO: update driver to expose this from INpgsqlTypeMapper.
+        [NotNull] static readonly INpgsqlNameTranslator DefaultNameTranslator = new NpgsqlSnakeCaseNameTranslator();
+
+        #region Sequences
+
         /// <summary>
-        ///     Configures the model to use a sequence-based hi-lo pattern to generate values for properties
-        ///     marked as <see cref="ValueGenerated.OnAdd" />, when targeting PostgreSQL.
+        /// Configures the model to use a sequence-based hi-lo pattern to generate values for properties
+        /// marked as <see cref="ValueGenerated.OnAdd" />, when targeting PostgreSQL.
         /// </summary>
-        /// <param name="modelBuilder"> The model builder. </param>
-        /// <param name="name"> The name of the sequence. </param>
-        /// <param name="schema">The schema of the sequence. </param>
-        /// <returns> The same builder instance so that multiple calls can be chained. </returns>
+        /// <param name="modelBuilder">The model builder.</param>
+        /// <param name="name">The name of the sequence.</param>
+        /// <param name="schema">The schema of the sequence.</param>
+        /// <returns>The same builder instance so that multiple calls can be chained.</returns>
         public static ModelBuilder ForNpgsqlUseSequenceHiLo(
             [NotNull] this ModelBuilder modelBuilder,
             [CanBeNull] string name = null,
@@ -45,12 +82,12 @@ namespace Microsoft.EntityFrameworkCore
         }
 
         /// <summary>
-        ///     Configures the model to use the PostgreSQL SERIAL feature to generate values for properties
-        ///     marked as <see cref="ValueGenerated.OnAdd" />, when targeting PostgreSQL. This is the default
-        ///     behavior when targeting PostgreSQL.
+        /// Configures the model to use the PostgreSQL SERIAL feature to generate values for properties
+        /// marked as <see cref="ValueGenerated.OnAdd" />, when targeting PostgreSQL. This is the default
+        /// behavior when targeting PostgreSQL.
         /// </summary>
-        /// <param name="modelBuilder"> The model builder. </param>
-        /// <returns> The same builder instance so that multiple calls can be chained. </returns>
+        /// <param name="modelBuilder">The model builder.</param>
+        /// <returns>The same builder instance so that multiple calls can be chained.</returns>
         public static ModelBuilder ForNpgsqlUseSerialColumns(
             [NotNull] this ModelBuilder modelBuilder)
         {
@@ -64,6 +101,8 @@ namespace Microsoft.EntityFrameworkCore
 
             return modelBuilder;
         }
+
+        #endregion
 
         #region Identity
 
@@ -134,6 +173,8 @@ namespace Microsoft.EntityFrameworkCore
 
         #endregion Identity
 
+        #region Extensions
+
         public static ModelBuilder HasPostgresExtension(
             [NotNull] this ModelBuilder modelBuilder,
             [NotNull] string name)
@@ -145,6 +186,25 @@ namespace Microsoft.EntityFrameworkCore
             return modelBuilder;
         }
 
+        #endregion
+
+        #region Enums
+
+        /// <summary>
+        /// Registers a user-defined enum type in the model.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder in which to create the enum type.</param>
+        /// <param name="schema">The schema in which to create the enum type.</param>
+        /// <param name="name">The name of the enum type to create.</param>
+        /// <param name="labels">The enum label values.</param>
+        /// <returns>
+        /// The updated <see cref="ModelBuilder"/>.
+        /// </returns>
+        /// <remarks>
+        /// See: https://www.postgresql.org/docs/current/static/datatype-enum.html
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">builder</exception>
+        [NotNull]
         public static ModelBuilder ForNpgsqlHasEnum(
             [NotNull] this ModelBuilder modelBuilder,
             [CanBeNull] string schema,
@@ -159,11 +219,64 @@ namespace Microsoft.EntityFrameworkCore
             return modelBuilder;
         }
 
+        /// <summary>
+        /// Registers a user-defined enum type in the model.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder in which to create the enum type.</param>
+        /// <param name="name">The name of the enum type to create.</param>
+        /// <param name="labels">The enum label values.</param>
+        /// <returns>
+        /// The updated <see cref="ModelBuilder"/>.
+        /// </returns>
+        /// <remarks>
+        /// See: https://www.postgresql.org/docs/current/static/datatype-enum.html
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">builder</exception>
+        [NotNull]
         public static ModelBuilder ForNpgsqlHasEnum(
             [NotNull] this ModelBuilder modelBuilder,
             [NotNull] string name,
             [NotNull] string[] labels)
-            => modelBuilder.ForNpgsqlHasEnum(null, name, labels);
+            => ForNpgsqlHasEnum(modelBuilder, null, name, labels);
+
+        /// <summary>
+        /// Registers a user-defined enum type in the model.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder in which to create the enum type.</param>
+        /// <param name="schema">The schema in which to create the enum type.</param>
+        /// <param name="name">The name of the enum type to create.</param>
+        /// <param name="nameTranslator">
+        /// The translator for name and label inference.
+        /// Defaults to <see cref="NpgsqlSnakeCaseNameTranslator"/>.</param>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <returns>
+        /// The updated <see cref="ModelBuilder"/>.
+        /// </returns>
+        /// <remarks>
+        /// See: https://www.postgresql.org/docs/current/static/datatype-enum.html
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">builder</exception>
+        [NotNull]
+        public static ModelBuilder ForNpgsqlHasEnum<TEnum>(
+            [NotNull] this ModelBuilder modelBuilder,
+            [CanBeNull] string schema = null,
+            [CanBeNull] string name = null,
+            [CanBeNull] INpgsqlNameTranslator nameTranslator = null)
+            where TEnum : struct, Enum
+        {
+            if (nameTranslator == null)
+                nameTranslator = DefaultNameTranslator;
+
+            return ForNpgsqlHasEnum(
+                modelBuilder,
+                schema,
+                name ?? GetTypePgName<TEnum>(nameTranslator),
+                GetMemberPgNames<TEnum>(nameTranslator));
+        }
+
+        #endregion
+
+        #region Templates
 
         public static ModelBuilder HasDatabaseTemplate(
             [NotNull] this ModelBuilder modelBuilder,
@@ -175,6 +288,10 @@ namespace Microsoft.EntityFrameworkCore
             modelBuilder.Model.Npgsql().DatabaseTemplate = templateDatabaseName;
             return modelBuilder;
         }
+
+        #endregion
+
+        #region Ranges
 
         /// <summary>
         /// Registers a user-defined range type in the model.
@@ -238,6 +355,10 @@ namespace Microsoft.EntityFrameworkCore
             [NotNull] string subtype)
             => ForNpgsqlHasRange(modelBuilder, null, name, subtype);
 
+        #endregion
+
+        #region Tablespaces
+
         public static ModelBuilder ForNpgsqlUseTablespace(
             [NotNull] this ModelBuilder modelBuilder,
             [NotNull] string tablespace)
@@ -248,5 +369,29 @@ namespace Microsoft.EntityFrameworkCore
             modelBuilder.Model.Npgsql().Tablespace = tablespace;
             return modelBuilder;
         }
+
+        #endregion
+
+        #region Helpers
+
+        // See: https://github.com/npgsql/npgsql/blob/dev/src/Npgsql/TypeMapping/TypeMapperBase.cs#L132-L138
+        [NotNull]
+        static string GetTypePgName<TEnum>([NotNull] INpgsqlNameTranslator nameTranslator) where TEnum : struct, Enum
+            => typeof(TEnum).GetCustomAttribute<PgNameAttribute>()?.PgName
+               ??
+               nameTranslator.TranslateTypeName(typeof(TEnum).Name);
+
+        // See: https://github.com/npgsql/npgsql/blob/dev/src/Npgsql/TypeHandlers/EnumHandler.cs#L118-L129
+        [NotNull]
+        [ItemNotNull]
+        static string[] GetMemberPgNames<TEnum>([NotNull] INpgsqlNameTranslator nameTranslator) where TEnum : struct, Enum
+            => typeof(TEnum).GetFields(BindingFlags.Static | BindingFlags.Public)
+                            .Select(x =>
+                                x.GetCustomAttribute<PgNameAttribute>()?.PgName
+                                ??
+                                nameTranslator.TranslateMemberName(x.Name))
+                            .ToArray();
+
+        #endregion
     }
 }
