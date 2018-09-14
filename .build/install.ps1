ae54a3f955445a35c432131e87a2fdb9f20890f7
Austin Drenski <austin.drenski@gmail.com>
2018-09-14 00:53:45 -0400

    Merge `install.sh` into `install.ps1`

--- .build/install.ps1
+++ .build/install.ps1
@@ -1,16 +1,31 @@
 #!/usr/bin/pwsh
 
+If ($isLinux) {
+    $env:DEBIAN_FRONTEND='noninteractive'
+    lsb_release -irc
+}
+
 ###################
 ## Setup PostGIS ##
 ###################
 
 If ($isWindows) {
     If (!(Test-Path $env:POSTGIS_EXE)) {
-        Write-Host Downloading PostGIS...
+        Write-Host 'Downloading PostGIS'
         (New-Object Net.WebClient).DownloadFile("http://download.osgeo.org/postgis/windows/pg10/$env:POSTGIS_EXE", "$env:POSTGIS_EXE")
     }
-    Write-Host Installing PostGIS...
+    Write-Host 'Installing PostGIS'
     iex ".\$env:POSTGIS_EXE /S /D='C:\Program Files\PostgreSQL\10'"
+}
+If ($isLinux) {
+    Write-Host 'Registering PostgreSQL Apt Repository'
+    sudo sh -c 'echo ""deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main"" >> /etc/apt/sources.list'
+    wget -q -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+    sudo apt-get -qq update
+
+    Write-Host 'Installing PostGIS'
+    sudo apt-get -qq install postgresql-10-postgis-2.4 > /dev/null
+    sudo apt-get -qq install postgresql-10-postgis-2.4-scripts > /dev/null
 }
 
 #####################
@@ -22,6 +37,11 @@ If ($isWindows) {
     # - ps: Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1" -OutFile "install-dotnet.ps1"
     # - ps: .\install-dotnet.ps1 -Version 2.1.300-rc1-008673
     # - ps: $blockRdp = $true; iex ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+}
+If ($isLinux) {
+    ## This adds ~4 minutes to the build time.
+    # Write-Host 'Upgrading .NET Core SDK'
+    # sudo apt-get -qq upgrade dotnet-sdk-2.1 > /dev/null
 }
 
 ###########################
