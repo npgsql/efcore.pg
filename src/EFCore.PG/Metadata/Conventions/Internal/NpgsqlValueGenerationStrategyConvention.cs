a9bc983d016a353bcfae4bf3fb092e86ae201bb9
Austin Drenski <austin.drenski@gmail.com>
2018-10-21 13:16:35 -0400

    Remove file license blocks per discussion in 672
    See: 672#issuecomment-431682630

--- src/EFCore.PG/Metadata/Conventions/Internal/NpgsqlValueGenerationStrategyConvention.cs
+++ src/EFCore.PG/Metadata/Conventions/Internal/NpgsqlValueGenerationStrategyConvention.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-using Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.Internal;
 
