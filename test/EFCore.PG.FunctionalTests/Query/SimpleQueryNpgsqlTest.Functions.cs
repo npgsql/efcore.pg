dda1e470c2bff83b817d7b65840b0bcefe233352
Austin Drenski <austin.drenski@gmail.com>
2018-11-17 12:56:41 -0500

    Fix test (omitted entry count is needed)

--- test/EFCore.PG.FunctionalTests/Query/SimpleQueryNpgsqlTest.Functions.cs
+++ test/EFCore.PG.FunctionalTests/Query/SimpleQueryNpgsqlTest.Functions.cs
@@ -201,7 +201,7 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query
         [MemberData(nameof(IsAsyncData))]
         public virtual async Task OrderBy_Guid_NewGuid(bool isAsync)
         {
-            await AssertQuery<OrderDetail>(isAsync, ods => ods.OrderBy(od => Guid.NewGuid()));
+            await AssertQuery<OrderDetail>(isAsync, ods => ods.OrderBy(od => Guid.NewGuid()), entryCount: 2155);
             AssertContainsSqlFragment("ORDER BY uuid_generate_v4()");
         }
 
