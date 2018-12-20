e81a2415c94b410510a76c057606ed3a73965065
Austin Drenski <austin.drenski@gmail.com>
2018-12-20 12:06:23 -0500

    Replace IModelValidator with NpgsqlModelValidator

---
+++ src/EFCore.PG/Infrastructure/NpgsqlModelValidator.cs
@@ -0,0 +1,64 @@
+using System;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.Internal;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Utilities;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure
+{
+    /// <summary>
+    /// The validator that enforces rules for Npgsql.
+    /// </summary>
+    public class NpgsqlModelValidator : RelationalModelValidator
+    {
+        /// <summary>
+        /// The backend version to target.
+        /// </summary>
+        [CanBeNull] readonly Version _postgresVersion;
+
+        /// <inheritdoc />
+        public NpgsqlModelValidator(
+            [NotNull] ModelValidatorDependencies dependencies,
+            [NotNull] RelationalModelValidatorDependencies relationalDependencies,
+            [NotNull] INpgsqlOptions npgsqlOptions)
+            : base(dependencies, relationalDependencies)
+            => _postgresVersion = Check.NotNull(npgsqlOptions, nameof(npgsqlOptions)).PostgresVersion;
+
+        public override void Validate(IModel model)
+        {
+            base.Validate(model);
+            ValidateIdentityUsage(model);
+        }
+
+        #region Npgsql-specific validations
+
+        /// <summary>
+        /// Validates that identity columns are used only with PostgreSQL 10.0 or later.
+        /// </summary>
+        /// <param name="model">The model to validate.</param>
+        protected virtual void ValidateIdentityUsage([NotNull] IModel model)
+        {
+            if (VersionAtLeast(10, 0))
+                return;
+
+            var strategy = model.Npgsql().ValueGenerationStrategy;
+
+            if (strategy == NpgsqlValueGenerationStrategy.IdentityAlwaysColumn ||
+                strategy == NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+            {
+                throw new InvalidOperationException($"'{strategy}' requires PostgreSQL 10.0 or later.");
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        bool VersionAtLeast(int major, int minor) => _postgresVersion is null || new Version(major, minor) <= _postgresVersion;
+
+        #endregion
+    }
+}
