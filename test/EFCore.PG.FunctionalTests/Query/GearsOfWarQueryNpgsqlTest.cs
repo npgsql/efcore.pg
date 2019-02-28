b8e98e3d4c3806d6ac13e94f1fd20873992f8b52
Shay Rojansky <roji@roji.org>
2019-02-28 16:57:53 +0100

    Ignore additional DateTimeOffset tests
    Since PostgreSQL has no type which stores the timezone.

--- test/EFCore.PG.FunctionalTests/Query/GearsOfWarQueryNpgsqlTest.cs
+++ test/EFCore.PG.FunctionalTests/Query/GearsOfWarQueryNpgsqlTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Query;
 using Xunit.Abstractions;
 
@@ -12,19 +12,32 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query
             : base(fixture)
             => Fixture.TestSqlLoggerFactory.Clear();
 
+        #region Ignore DateTimeOffset tests
+
         // PostgreSQL has no datatype that corresponds to DateTimeOffset.
         // DateTimeOffset gets mapped to "timestamptz" which does not record the offset, so the values coming
         // back from the database aren't as expected.
+
+        public override Task DateTimeOffset_DateAdd_AddDays(bool isAsync) => Task.CompletedTask;
+        public override Task DateTimeOffset_DateAdd_AddHours(bool isAsync) => Task.CompletedTask;
+        public override Task DateTimeOffset_DateAdd_AddMilliseconds(bool isAsync) => Task.CompletedTask;
+        public override Task DateTimeOffset_DateAdd_AddMinutes(bool isAsync) => Task.CompletedTask;
+        public override Task DateTimeOffset_DateAdd_AddMonths(bool isAsync) => Task.CompletedTask;
+        public override Task DateTimeOffset_DateAdd_AddSeconds(bool isAsync) => Task.CompletedTask;
+        public override Task DateTimeOffset_DateAdd_AddYears(bool isAsync) => Task.CompletedTask;
+
+        public override Task Where_datetimeoffset_date_component(bool isAsync) => Task.CompletedTask;
+        public override Task Where_datetimeoffset_day_component(bool isAsync) => Task.CompletedTask;
+        public override Task Where_datetimeoffset_dayofyear_component(bool isAsync) => Task.CompletedTask;
         public override Task Where_datetimeoffset_hour_component(bool isAsync) => Task.CompletedTask;
-
-        // PostgreSQL has no datatype that corresponds to DateTimeOffset.
-        // DateTimeOffset gets mapped to "timestamptz" which does not record the offset, so the values coming
-        // back from the database aren't as expected.
         public override Task Where_datetimeoffset_minute_component(bool isAsync) => Task.CompletedTask;
-
-        // PostgreSQL has no datatype that corresponds to DateTimeOffset.
-        // DateTimeOffset gets mapped to "timestamptz" which does not record the offset, so the values coming
-        // back from the database aren't as expected.
+        public override Task Where_datetimeoffset_month_component(bool isAsync) => Task.CompletedTask;
+        public override Task Where_datetimeoffset_second_component(bool isAsync) => Task.CompletedTask;
         public override Task Time_of_day_datetimeoffset(bool isAsync) => Task.CompletedTask;
+
+        public override Task Where_datetimeoffset_now(bool isAsync) => Task.CompletedTask;
+        public override Task Where_datetimeoffset_utcnow(bool isAsync) => Task.CompletedTask;
+
+        #endregion Ignore DateTimeOffset tests
     }
 }
