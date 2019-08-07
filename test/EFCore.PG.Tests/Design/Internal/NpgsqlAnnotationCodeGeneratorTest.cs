iulian-popescu/Npgsql.EntityFrameworkCore.PostgreSQL

Make identity the default value generation strategy
Closes #804

test/EFCore.PG.Tests/Design/Internal/NpgsqlAnnotationCodeGeneratorTest.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.Conventions;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.Internal;
 using Xunit;
@@ -10,23 +12,84 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Design.Internal
     public class NpgsqlAnnotationCodeGeneratorTest
     {
         [Fact]
-        public void GenerateFluentApi_identity()
+        public void GenerateFluentApi_value_generation()
         {
-            var generator = new NpgsqlAnnotationCodeGenerator(new AnnotationCodeGeneratorDependencies());
+            var defaultOptions = new DbContextOptionsBuilder().Options;
+            var generator = new NpgsqlAnnotationCodeGenerator(new AnnotationCodeGeneratorDependencies(), defaultOptions);
             var modelBuilder = new ModelBuilder(NpgsqlConventionSetBuilder.Build());
             modelBuilder.Entity(
                 "Post",
                 x =>
                 {
-                    x.Property<int>("Id").UseIdentityAlwaysColumn();
+                    x.Property<int>("Id1").UseIdentityByDefaultColumn();
+                    x.Property<int>("Id2").UseIdentityAlwaysColumn();
+                    x.Property<int>("Id3").UseSerialColumn();
                 });
-            var property = modelBuilder.Model.FindEntityType("Post").GetProperties().Single();
+
+            var property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "Id1");
             var annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
-
+            Assert.True(generator.IsHandledByConvention(property, annotation));
             var result = generator.GenerateFluentApi(property, annotation);
+            Assert.Equal("UseIdentityByDefaultColumn", result.Method);
+            Assert.Equal(0, result.Arguments.Count);
 
+            property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "Id2");
+            annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
+            Assert.False(generator.IsHandledByConvention(property, annotation));
+            result = generator.GenerateFluentApi(property, annotation);
             Assert.Equal("UseIdentityAlwaysColumn", result.Method);
+            Assert.Equal(0, result.Arguments.Count);
 
+            property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "Id3");
+            annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
+            Assert.False(generator.IsHandledByConvention(property, annotation));
+            result = generator.GenerateFluentApi(property, annotation);
+            Assert.Equal("UseSerialColumn", result.Method);
+            Assert.Equal(0, result.Arguments.Count);
+        }
+
+        [Fact]
+        public void GenerateFluentApi_value_generation_old_database()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            new NpgsqlDbContextOptionsBuilder(optionsBuilder).SetPostgresVersion(9, 6);
+            var options = optionsBuilder.Options;
+            var generator = new NpgsqlAnnotationCodeGenerator(new AnnotationCodeGeneratorDependencies(), options);
+            var modelBuilder = new ModelBuilder(NpgsqlConventionSetBuilder.Build());
+            modelBuilder.Entity(
+                "Post",
+                x =>
+                {
+                    x.Property<int>("Id1").UseIdentityByDefaultColumn();
+                    x.Property<int>("Id2").UseIdentityAlwaysColumn();
+                    x.Property<int>("Id3").UseSerialColumn();
+                });
+
+            var property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "Id1");
+            var annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
+            Assert.False(generator.IsHandledByConvention(property, annotation));
+            var result = generator.GenerateFluentApi(property, annotation);
+            Assert.Equal("UseIdentityByDefaultColumn", result.Method);
+            Assert.Equal(0, result.Arguments.Count);
+
+            property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "Id2");
+            annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
+            Assert.False(generator.IsHandledByConvention(property, annotation));
+            result = generator.GenerateFluentApi(property, annotation);
+            Assert.Equal("UseIdentityAlwaysColumn", result.Method);
+            Assert.Equal(0, result.Arguments.Count);
+
+            property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "Id3");
+            annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
+            Assert.True(generator.IsHandledByConvention(property, annotation));
+            result = generator.GenerateFluentApi(property, annotation);
+            Assert.Equal("UseSerialColumn", result.Method);
             Assert.Equal(0, result.Arguments.Count);
         }
     }

test/EFCore.PG.Tests/Metadata/NpgsqlMetadataExtensionsTest.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Internal;
 using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
 using Xunit;
 
@@ -231,7 +229,8 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Metadata
             var modelBuilder = GetModelBuilder();
             var model = modelBuilder.Model;
 
-            Assert.Equal(NpgsqlValueGenerationStrategy.SerialColumn, model.GetNpgsqlValueGenerationStrategy());
+            // TODO for PG9.6 testing: make this conditional
+            Assert.Equal(NpgsqlValueGenerationStrategy.IdentityByDefaultColumn, model.GetNpgsqlValueGenerationStrategy());
 
             model.SetNpgsqlValueGenerationStrategy(NpgsqlValueGenerationStrategy.SequenceHiLo);
 
@@ -309,7 +308,8 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Metadata
                 .Property(e => e.NullableInt).ValueGeneratedOnAdd()
                 .Metadata;
 
-            Assert.Equal(NpgsqlValueGenerationStrategy.SerialColumn, property.GetNpgsqlValueGenerationStrategy());
+            // TODO for PG9.6 testing: make this conditional
+            Assert.Equal(NpgsqlValueGenerationStrategy.IdentityByDefaultColumn, property.GetNpgsqlValueGenerationStrategy());
 
             property.SetNpgsqlValueGenerationStrategy(NpgsqlValueGenerationStrategy.SequenceHiLo);
 
@@ -317,7 +317,7 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Metadata
 
             property.SetNpgsqlValueGenerationStrategy(null);
 
-            Assert.Equal(NpgsqlValueGenerationStrategy.SerialColumn, property.GetNpgsqlValueGenerationStrategy());
+            Assert.Equal(NpgsqlValueGenerationStrategy.IdentityByDefaultColumn, property.GetNpgsqlValueGenerationStrategy());
         }
 
         [ConditionalFact]
@@ -381,6 +381,7 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Metadata
 
             Assert.Null(property.FindNpgsqlHiLoSequence());
 
+            // TODO for PG9.6 testing: make this conditional
             modelBuilder.Model.SetNpgsqlValueGenerationStrategy(NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
 
             Assert.Null(property.FindNpgsqlHiLoSequence());

npgsql/efcore.pg

test/EFCore.PG.Tests/Design/Internal/NpgsqlAnnotationCodeGeneratorTest.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.Conventions;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.Internal;
 using Xunit;
@@ -10,7 +12,7 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Design.Internal
     public class NpgsqlAnnotationCodeGeneratorTest
     {
         [Fact]
-        public void GenerateFluentApi_identity()
+        public void GenerateFluentApi_value_generation()
         {
             var generator = new NpgsqlAnnotationCodeGenerator(new AnnotationCodeGeneratorDependencies());
             var modelBuilder = new ModelBuilder(NpgsqlConventionSetBuilder.Build());
@@ -18,15 +20,38 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Design.Internal
                 "Post",
                 x =>
                 {
-                    x.Property<int>("Id").UseIdentityAlwaysColumn();
+                    x.Property<int>("IdentityByDefault").UseIdentityByDefaultColumn();
+                    x.Property<int>("IdentityAlways").UseIdentityAlwaysColumn();
+                    x.Property<int>("Serial").UseSerialColumn();
                 });
-            var property = modelBuilder.Model.FindEntityType("Post").GetProperties().Single();
+
+            // Note: both serial and identity-by-default columns are considered by-convention - we don't want
+            // to assume that the PostgreSQL version of the scaffolded database necessarily determines the
+            // version of the database that the scaffolded model will target. This makes life difficult for
+            // models with mixed strategies but that's an edge case.
+
+            var property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "IdentityByDefault");
             var annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
-
+            Assert.True(generator.IsHandledByConvention(property, annotation));
             var result = generator.GenerateFluentApi(property, annotation);
+            Assert.Equal("UseIdentityByDefaultColumn", result.Method);
+            Assert.Equal(0, result.Arguments.Count);
 
+            property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "IdentityAlways");
+            annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
+            Assert.False(generator.IsHandledByConvention(property, annotation));
+            result = generator.GenerateFluentApi(property, annotation);
             Assert.Equal("UseIdentityAlwaysColumn", result.Method);
+            Assert.Equal(0, result.Arguments.Count);
 
+            property = modelBuilder.Model.FindEntityType("Post").GetProperties()
+                .Single(p => p.Name == "Serial");
+            annotation = property.FindAnnotation(NpgsqlAnnotationNames.ValueGenerationStrategy);
+            Assert.True(generator.IsHandledByConvention(property, annotation));
+            result = generator.GenerateFluentApi(property, annotation);
+            Assert.Equal("UseSerialColumn", result.Method);
             Assert.Equal(0, result.Arguments.Count);
         }
     }

test/EFCore.PG.Tests/Metadata/NpgsqlMetadataExtensionsTest.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Internal;
 using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
 using Xunit;
 
@@ -231,7 +229,8 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Metadata
             var modelBuilder = GetModelBuilder();
             var model = modelBuilder.Model;
 
-            Assert.Equal(NpgsqlValueGenerationStrategy.SerialColumn, model.GetValueGenerationStrategy());
+            // TODO for PG9.6 testing: make this conditional
+            Assert.Equal(NpgsqlValueGenerationStrategy.IdentityByDefaultColumn, model.GetValueGenerationStrategy());
 
             model.SetValueGenerationStrategy(NpgsqlValueGenerationStrategy.SequenceHiLo);
 
@@ -309,7 +308,8 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Metadata
                 .Property(e => e.NullableInt).ValueGeneratedOnAdd()
                 .Metadata;
 
-            Assert.Equal(NpgsqlValueGenerationStrategy.SerialColumn, property.GetValueGenerationStrategy());
+            // TODO for PG9.6 testing: make this conditional
+            Assert.Equal(NpgsqlValueGenerationStrategy.IdentityByDefaultColumn, property.GetValueGenerationStrategy());
 
             property.SetValueGenerationStrategy(NpgsqlValueGenerationStrategy.SequenceHiLo);
 
@@ -317,7 +317,8 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Metadata
 
             property.SetValueGenerationStrategy(null);
 
-            Assert.Equal(NpgsqlValueGenerationStrategy.SerialColumn, property.GetValueGenerationStrategy());
+            // TODO for PG9.6 testing: make this conditional
+            Assert.Equal(NpgsqlValueGenerationStrategy.IdentityByDefaultColumn, property.GetValueGenerationStrategy());
         }
 
         [ConditionalFact]
@@ -381,6 +382,7 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Metadata
 
             Assert.Null(property.FindHiLoSequence());
 
+            // TODO for PG9.6 testing: make this conditional
             modelBuilder.Model.SetValueGenerationStrategy(NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
 
             Assert.Null(property.FindHiLoSequence());