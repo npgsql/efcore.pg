b4b15828e1ff75145d8ba2f153b2dcac6a1a4116
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2019-11-09 16:11:03 -0500

    Added fuzzystrmatch module plugin

---
+++ src/EFCore.PG.FuzzyStringMatch/Extensions/NpgsqlFuzzyStringMatchDbFunctionsExtensions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class NpgsqlFuzzyStringMatchDbFunctionsExtensions
+    {
+        /// <summary>
+        /// The soundex function converts a string to its Soundex code.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>soundex(text)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static string FuzzyStringMatchSoundex(this DbFunctions _, string text) =>
+            throw new NotSupportedException();
+
+        /// <summary>
+        /// The difference function converts two strings to their Soundex codes and
+        /// then returns the number of matching code positions. Since Soundex codes
+        /// have four characters, the result ranges from zero to four, with zero being
+        /// no match and four being an exact match.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>difference(source, target)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static int FuzzyStringMatchDifference(this DbFunctions _, string source, string target) =>
+            throw new NotSupportedException();
+
+        /// <summary>
+        /// Returns the Levenshtein distance between two strings.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>levenshtein(source, target)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static int FuzzyStringMatchLevenshtein(this DbFunctions _, string source, string target) =>
+            throw new NotSupportedException();
+
+        /// <summary>
+        /// Returns the Levenshtein distance between two strings.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>levenshtein(source, target, ins_cost, del_cost, sub_cost)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static int FuzzyStringMatchLevenshtein(this DbFunctions _, string source, string target, int ins_cost, int del_cost, int sub_cost) =>
+            throw new NotSupportedException();
+
+        /// <summary>
+        /// levenshtein_less_equal is an accelerated version of the Levenshtein function for use when only small distances are of interest.
+        /// If the actual distance is less than or equal to max_d, then levenshtein_less_equal returns the correct distance;
+        /// otherwise it returns some value greater than max_d. If max_d is negative then the behavior is the same as levenshtein.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>levenshtein_less_equal(source, target, max_d)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static int FuzzyStringMatchLevenshteinLessEqual(this DbFunctions _, string source, string target, int max_d) =>
+            throw new NotSupportedException();
+
+        /// <summary>
+        /// levenshtein_less_equal is an accelerated version of the Levenshtein function for use when only small distances are of interest.
+        /// If the actual distance is less than or equal to max_d, then levenshtein_less_equal returns the correct distance;
+        /// otherwise it returns some value greater than max_d. If max_d is negative then the behavior is the same as levenshtein.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>levenshtein_less_equal(source, target, ins_cost, del_cost, sub_cost, max_d)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static int FuzzyStringMatchLevenshteinLessEqual(this DbFunctions _, string source, string target, int ins_cost, int del_cost, int sub_cost, int max_d) =>
+            throw new NotSupportedException();
+
+        /// <summary>
+        /// The metaphone function converts a string to its Metaphone code.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>metaphone(text, max_output_length)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static string FuzzyStringMatchMetaphone(this DbFunctions _, string text, int max_output_length) =>
+            throw new NotSupportedException();
+
+        /// <summary>
+        /// The dmetaphone function converts a string to its primary Double Metaphone code.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>dmetaphone(text)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static string FuzzyStringMatchDoubleMetaphone(this DbFunctions _, string text) =>
+            throw new NotSupportedException();
+
+        /// <summary>
+        /// The dmetaphone_alt function converts a string to its alternate Double Metaphone code.
+        /// </summary>
+        /// <remarks>
+        /// The method call is translated to <c>dmetaphone_alt(text)</c>.
+        ///
+        /// See https://www.postgresql.org/docs/current/fuzzystrmatch.html.
+        /// </remarks>
+        public static string FuzzyStringMatchDoubleMetaphoneAlt(this DbFunctions _, string text) =>
+            throw new NotSupportedException();
+    }
+}
---
+++ src/EFCore.PG.FuzzyStringMatch/Query/ExpressionTranslators/Internal/NpgsqlFuzzyStringMatchMethodTranslator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Expressions.Internal;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Internal;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query.ExpressionTranslators.Internal
+{
+    public class NpgsqlFuzzyStringMatchMethodTranslator : IMethodCallTranslator
+    {
+        static readonly Dictionary<MethodInfo, string> Functions = new Dictionary<MethodInfo, string>
+        {
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchSoundex), new[] { typeof(DbFunctions), typeof(string) })] = "soundex",
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchDifference), new[] { typeof(DbFunctions), typeof(string), typeof(string) })] = "difference",
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchLevenshtein), new[] { typeof(DbFunctions), typeof(string), typeof(string) })] = "levenshtein",
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchLevenshtein), new[] { typeof(DbFunctions), typeof(string), typeof(string), typeof(int), typeof(int), typeof(int) })] = "levenshtein",
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchLevenshteinLessEqual), new[] { typeof(DbFunctions), typeof(string), typeof(string), typeof(int) })] = "levenshtein_less_equal",
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchLevenshteinLessEqual), new[] { typeof(DbFunctions), typeof(string), typeof(string), typeof(int), typeof(int), typeof(int), typeof(int) })] = "levenshtein_less_equal",
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchMetaphone), new[] { typeof(DbFunctions), typeof(string), typeof(int) })] = "metaphone",
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchDoubleMetaphone), new[] { typeof(DbFunctions), typeof(string) })] = "dmetaphone",
+            [GetRuntimeMethod(nameof(NpgsqlFuzzyStringMatchDbFunctionsExtensions.FuzzyStringMatchDoubleMetaphoneAlt), new[] { typeof(DbFunctions), typeof(string) })] = "dmetaphone_alt"
+        };
+
+        static MethodInfo GetRuntimeMethod(string name, params Type[] parameters)
+            => typeof(NpgsqlFuzzyStringMatchDbFunctionsExtensions).GetRuntimeMethod(name, parameters);
+
+        readonly NpgsqlSqlExpressionFactory _sqlExpressionFactory;
+
+        public NpgsqlFuzzyStringMatchMethodTranslator(NpgsqlSqlExpressionFactory sqlExpressionFactory, IRelationalTypeMappingSource typeMappingSource)
+        {
+            _sqlExpressionFactory = sqlExpressionFactory;
+        }
+
+        /// <inheritdoc />
+        public SqlExpression Translate(SqlExpression instance, MethodInfo method, IReadOnlyList<SqlExpression> arguments)
+        {
+            if (Functions.TryGetValue(method, out var function))
+                return _sqlExpressionFactory.Function(function, arguments.Skip(1), method.ReturnType);
+
+            return null;
+        }
+    }
+}
