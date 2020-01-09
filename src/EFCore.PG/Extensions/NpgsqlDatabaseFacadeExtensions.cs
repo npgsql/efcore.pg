b3a2cb1971017a921e9605f9ef910a7cee8da1e7
Shay Rojansky <roji@roji.org>
2020-01-09 18:36:19 +0100

    Tiny cleanup of IsNpgsql

--- src/EFCore.PG/Extensions/NpgsqlDatabaseFacadeExtensions.cs
+++ src/EFCore.PG/Extensions/NpgsqlDatabaseFacadeExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.Internal;
@@ -8,26 +7,24 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.Internal;
 namespace Microsoft.EntityFrameworkCore
 {
     /// <summary>
-    ///     Npgsql specific extension methods for <see cref="DbContext.Database" />.
+    /// Npgsql specific extension methods for <see cref="DbContext.Database" />.
     /// </summary>
     public static class NpgsqlDatabaseFacadeExtensions
     {
         /// <summary>
-        ///     <para>
-        ///         Returns true if the database provider currently in use is the Npgsql provider.
-        ///     </para>
-        ///     <para>
-        ///         This method can only be used after the <see cref="DbContext" /> has been configured because
-        ///         it is only then that the provider is known. This means that this method cannot be used
-        ///         in <see cref="DbContext.OnConfiguring" /> because this is where application code sets the
-        ///         provider to use as part of configuring the context.
-        ///     </para>
+        /// <para>
+        /// Returns true if the database provider currently in use is the Npgsql provider.
+        /// </para>
+        /// <para>
+        /// This method can only be used after the <see cref="DbContext" /> has been configured because
+        /// it is only then that the provider is known. This means that this method cannot be used
+        /// in <see cref="DbContext.OnConfiguring" /> because this is where application code sets the
+        /// provider to use as part of configuring the context.
+        /// </para>
         /// </summary>
-        /// <param name="database"> The facade from <see cref="DbContext.Database" />. </param>
-        /// <returns> True if SQL Server is being used; false otherwise. </returns>
+        /// <param name="database">The facade from <see cref="DbContext.Database" />.</param>
+        /// <returns>True if Npgsql is being used; false otherwise.</returns>
         public static bool IsNpgsql([NotNull] this DatabaseFacade database)
-            => database.ProviderName.Equals(
-                typeof(NpgsqlOptionsExtension).GetTypeInfo().Assembly.GetName().Name,
-                StringComparison.Ordinal);
+            => database.ProviderName == typeof(NpgsqlOptionsExtension).GetTypeInfo().Assembly.GetName().Name;
     }
 }
