6dd4ace14f9a996345ed932d9d4e3f38c96e945b
Shay Rojansky <roji@roji.org>
2020-08-13 10:16:24 +0300

    Map Northwind uint properties to int columns in tests
    As in SqlServer

--- test/EFCore.PG.FunctionalTests/Query/NorthwindQueryNpgsqlFixture.cs
+++ test/EFCore.PG.FunctionalTests/Query/NorthwindQueryNpgsqlFixture.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestModels.Northwind;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 using Npgsql.EntityFrameworkCore.PostgreSQL.TestModels.Northwind;
@@ -20,6 +21,29 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query
             var optionsBuilder = base.AddOptions(builder);
             new NpgsqlDbContextOptionsBuilder(optionsBuilder).ReverseNullOrdering();
             return optionsBuilder;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
+        {
+            base.OnModelCreating(modelBuilder, context);
+
+            // Note that we map price properties to numeric(12,2) columns, not to money as in SqlServer, since in
+            // PG, money is discouraged/obsolete.
+
+            modelBuilder.Entity<Employee>(
+                b =>
+                {
+                    b.Property(c => c.EmployeeID).HasColumnType("int");
+                    b.Property(c => c.ReportsTo).HasColumnType("int");
+                });
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.EmployeeID)
+                .HasColumnType("int");
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.UnitsInStock)
+                .HasColumnType("smallint");
         }
     }
 }
