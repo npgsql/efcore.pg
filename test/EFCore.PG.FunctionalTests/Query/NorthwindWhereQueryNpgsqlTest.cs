8cffcef78a25c0d864bb210caf424a7f335e04c3
Shay Rojansky <roji@roji.org>
2020-08-29 23:23:07 +0300

    Translate DateTime constructors
    Closes #1204

--- test/EFCore.PG.FunctionalTests/Query/NorthwindWhereQueryNpgsqlTest.cs
+++ test/EFCore.PG.FunctionalTests/Query/NorthwindWhereQueryNpgsqlTest.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestModels.Northwind;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Xunit;
 using Xunit.Abstractions;
@@ -13,6 +16,36 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query
         {
             ClearLog();
             //Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual async Task Where_datetime_ctor1(bool async)
+        {
+            await AssertQuery(
+                async,
+                ss => ss.Set<Order>().Where(o =>
+                    new DateTime(o.OrderDate.Value.Year, o.OrderDate.Value.Month, 1) == new DateTime(1996, 9, 11)));
+
+            AssertSql(
+                @"SELECT o.""OrderID"", o.""CustomerID"", o.""EmployeeID"", o.""OrderDate""
+FROM ""Orders"" AS o
+WHERE MAKE_DATE(DATE_PART('year', o.""OrderDate"")::INT, DATE_PART('month', o.""OrderDate"")::INT, 1) = TIMESTAMP '1996-09-11 00:00:00'");
+        }
+
+        [ConditionalTheory]
+        [MemberData(nameof(IsAsyncData))]
+        public virtual async Task Where_datetime_ctor2(bool async)
+        {
+            await AssertQuery(
+                async,
+                ss => ss.Set<Order>().Where(o =>
+                    new DateTime(o.OrderDate.Value.Year, o.OrderDate.Value.Month, 1, 0, 0, 0) == new DateTime(1996, 9, 11)));
+
+            AssertSql(
+                @"SELECT o.""OrderID"", o.""CustomerID"", o.""EmployeeID"", o.""OrderDate""
+FROM ""Orders"" AS o
+WHERE MAKE_TIMESTAMP(DATE_PART('year', o.""OrderDate"")::INT, DATE_PART('month', o.""OrderDate"")::INT, 1, 0, 0, 0::double precision) = TIMESTAMP '1996-09-11 00:00:00'");
         }
 
         [Theory(Skip = "SQL translation not implemented, too annoying")]
