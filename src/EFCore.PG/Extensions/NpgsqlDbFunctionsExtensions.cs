13b85509b09e16cdfb8660a7537ceb7abaa26146
Shay Rojansky <roji@roji.org>
2020-09-10 17:14:23 +0300

    Add EF.Functions.Reverse(string)
    Closes #1495

--- src/EFCore.PG/Extensions/NpgsqlDbFunctionsExtensions.cs
+++ src/EFCore.PG/Extensions/NpgsqlDbFunctionsExtensions.cs
@@ -14,10 +14,10 @@ namespace Microsoft.EntityFrameworkCore
         /// <summary>
         /// An implementation of the PostgreSQL ILIKE operation, which is an insensitive LIKE.
         /// </summary>
-        /// <param name="_">The DbFunctions instance.</param>
+        /// <param name="_">The <see cref="DbFunctions"/> instance.</param>
         /// <param name="matchExpression">The string that is to be matched.</param>
         /// <param name="pattern">The pattern which may involve wildcards %,_,[,],^.</param>
-        /// <returns>true if there is a match.</returns>
+        /// <returns><see langword="true" /> if there is a match.</returns>
         public static bool ILike(
             [CanBeNull] this DbFunctions _,
             [CanBeNull] string matchExpression,
@@ -28,19 +28,28 @@ namespace Microsoft.EntityFrameworkCore
         /// <summary>
         /// An implementation of the PostgreSQL ILIKE operation, which is an insensitive LIKE.
         /// </summary>
-        /// <param name="_">The DbFunctions instance.</param>
+        /// <param name="_">The <see cref="DbFunctions"/> instance.</param>
         /// <param name="matchExpression">The string that is to be matched.</param>
         /// <param name="pattern">The pattern which may involve wildcards %,_,[,],^.</param>
         /// <param name="escapeCharacter">
         /// The escape character (as a single character string) to use in front of %,_,[,],^
         /// if they are not used as wildcards.
         /// </param>
-        /// <returns>true if there is a match.</returns>
+        /// <returns><see langword="true" /> if there is a match.</returns>
         public static bool ILike(
             [CanBeNull] this DbFunctions _,
             [CanBeNull] string matchExpression,
             [CanBeNull] string pattern,
             [CanBeNull] string escapeCharacter)
             => throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(ILike)));
+
+        /// <summary>
+        /// Reverses a string by calling PostgreSQL <c>reverse()</c>.
+        /// </summary>
+        /// <param name="_">The <see cref="DbFunctions"/> instance.</param>
+        /// <param name="value">The string that is to be reversed.</param>
+        /// <returns>The reversed string.</returns>
+        public static string Reverse([CanBeNull] this DbFunctions _, [CanBeNull] string value)
+            => throw new InvalidOperationException(CoreStrings.FunctionOnClient(nameof(Reverse)));
     }
 }
