cc179809be6a3675d4c1a4639ac6bbb4a4ed737e
David Roth <david.roth@fusonic.net>
2020-10-16 11:43:02 +0200

    Refactor build in data types test and add BuiltInDataTypesNpgsqlTestBase.

--- test/EFCore.PG.NodaTime.FunctionalTests/BuiltInDataTypesWithNodaTimePluginEnabledTest.cs
+++ test/EFCore.PG.NodaTime.FunctionalTests/BuiltInDataTypesWithNodaTimePluginEnabledTest.cs
@@ -1,20 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
-using Xunit;
-using Xunit.Abstractions;
+using static Npgsql.EntityFrameworkCore.PostgreSQL.NodaTimeTest;
 
 namespace Npgsql.EntityFrameworkCore.PostgreSQL
 {
-    public class BuiltInDataTypesWithNodaTimePluginEnabledTest : BuiltInDataTypesNpgsqlTest,
-        IClassFixture<BuiltInDataTypesWithNodaTimePluginEnabledTest.BuiltInDataTypesWithNodaTimePluginEnabledFixture>
+    public class BuiltInDataTypesWithNodaTimePluginEnabledTest : BuiltInDataTypesNpgsqlTestBase<BuiltInDataTypesWithNodaTimePluginEnabledTest.BuiltInDataTypesWithNodaTimePluginEnabledFixture>
     {
-        public BuiltInDataTypesWithNodaTimePluginEnabledTest(BuiltInDataTypesWithNodaTimePluginEnabledFixture fixture, ITestOutputHelper testOutputHelper)
-            : base(fixture, testOutputHelper)
-        { }
-
-        public class BuiltInDataTypesWithNodaTimePluginEnabledFixture : BuiltInDataTypesNpgsqlFixture
+        public BuiltInDataTypesWithNodaTimePluginEnabledTest(BuiltInDataTypesWithNodaTimePluginEnabledFixture fixture)
+            : base(fixture)
         {
+            Fixture.TestSqlLoggerFactory.Clear();
+        }
+
+        public class BuiltInDataTypesWithNodaTimePluginEnabledFixture : BuiltInDataTypesNpgsqlTestBaseFixture
+        {
+            protected override string StoreName => "BuiltInDataTypesWithNodaTimeEnabled";
+
             protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
                 => base.AddServices(serviceCollection).AddEntityFrameworkNpgsqlNodaTime();
 
@@ -24,6 +26,12 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL
                 new NpgsqlDbContextOptionsBuilder(optionsBuilder).UseNodaTime();
 
                 return optionsBuilder;
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
+            {
+                base.OnModelCreating(modelBuilder, context);
+                modelBuilder.Entity<NodaTimeTypes>();
             }
         }
     }
