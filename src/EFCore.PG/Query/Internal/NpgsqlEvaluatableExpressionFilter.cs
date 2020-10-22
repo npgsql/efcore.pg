f89e234c893eb9c06007ab89cf3e9d3efb28b88b
Shay Rojansky <roji@roji.org>
2020-10-22 16:24:13 +0300

    Flag some more DbFunction extensions as non-evaluatable
    Also arrange extensions in directories

--- src/EFCore.PG/Query/Internal/NpgsqlEvaluatableExpressionFilter.cs
+++ src/EFCore.PG/Query/Internal/NpgsqlEvaluatableExpressionFilter.cs
@@ -24,13 +24,20 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query.Internal
 
         public override bool IsEvaluatableExpression(Expression expression, IModel model)
         {
-            // Full text search
-            if (expression is MethodCallExpression e && (
-                e.Method == TsQueryParse || e.Method == TsVectorParse ||
-                e.Method.DeclaringType == typeof(NpgsqlFullTextSearchDbFunctionsExtensions) ||
-                e.Method.DeclaringType == typeof(NpgsqlFullTextSearchLinqExtensions)))
+            if (expression is MethodCallExpression e)
             {
-                return false;
+                var declaringType = e.Method.DeclaringType;
+
+                if (e.Method == TsQueryParse || e.Method == TsVectorParse ||
+                    declaringType == typeof(NpgsqlDbFunctionsExtensions) ||
+                    declaringType == typeof(NpgsqlFullTextSearchDbFunctionsExtensions) ||
+                    declaringType == typeof(NpgsqlFullTextSearchLinqExtensions) ||
+                    declaringType == typeof(NpgsqlNetworkDbFunctionsExtensions) ||
+                    declaringType == typeof(NpgsqlJsonDbFunctionsExtensions) ||
+                    declaringType == typeof(NpgsqlRangeDbFunctionsExtensions))
+                {
+                    return false;
+                }
             }
 
             return base.IsEvaluatableExpression(expression, model);
