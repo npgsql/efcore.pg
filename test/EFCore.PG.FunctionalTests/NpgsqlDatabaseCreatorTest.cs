e443964af2c76357d96b26b8b9fe95b9f0a01b68
Shay Rojansky <roji@roji.org>
2021-01-05 14:58:03 +0100

    Correct namespace

--- test/EFCore.PG.FunctionalTests/NpgsqlDatabaseCreatorTest.cs
+++ test/EFCore.PG.FunctionalTests/NpgsqlDatabaseCreatorTest.cs
@@ -3,18 +3,17 @@ using System.Data;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal;
 using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
 using Xunit;
 
 // ReSharper disable MethodHasAsyncOverload
-// ReSharper disable InconsistentNaming
-namespace Microsoft.EntityFrameworkCore
+namespace Npgsql.EntityFrameworkCore.PostgreSQL
 {
     public class NpgsqlDatabaseCreatorExistsTest : NpgsqlDatabaseCreatorTest
     {
