7cbb63be3660e74a77a37b54f1c71a69b70874da
Tibor Fülöp <tibest15@gmail.com>
2021-01-06 20:56:28 +0100

    Fix methods summary comments

--- src/EFCore.PG.NTS/Extensions/NpgsqlNetTopologySuiteGeometryExtensions.cs
+++ src/EFCore.PG.NTS/Extensions/NpgsqlNetTopologySuiteGeometryExtensions.cs
@@ -11,20 +11,20 @@ namespace NetTopologySuite.Geometries
         /// <summary>
         /// Tests whether the distance from this Geometry to another is less than or equal to a specified value.
         /// </summary>
-        /// <param name="geometry">the Geometry to check the distance to.</param>
-        /// <param name="anotherGeometry">the distance value to compare.</param>
-        /// <param name="distance">whether to use sphere or spheroid distance measurement.</param>
-        /// <param name="useSpheroid">true if the geometries are less than distance apart.</param>
+        /// <param name="geometry"></param>
+        /// <param name="anotherGeometry">the Geometry to check the distance to.</param>
+        /// <param name="distance">the distance value to compare.</param>
+        /// <param name="useSpheroid">whether to use sphere or spheroid distance measurement.</param>
         /// <returns>true if the geometries are less than distance apart.</returns>
         public static bool IsWithinDistance(this Geometry geometry, Geometry anotherGeometry, double distance, bool useSpheroid) => default;
 
         /// <summary>
         /// Returns the minimum distance between this Geometry and another Geometry g.
         /// </summary>
-        /// <param name="geometry">the Geometry from which to compute the distance.</param>
-        /// <param name="anotherGeometry">whether to use sphere or spheroid distance measurement.</param>
-        /// <param name="useSpheroid">the distance between the geometries</param>
-        /// <returns>the distance between the geometries</returns>
+        /// <param name="geometry"></param>
+        /// <param name="anotherGeometry">the Geometry from which to compute the distance.</param>
+        /// <param name="useSpheroid">whether to use sphere or spheroid distance measurement.</param>
+        /// <returns>the distance between the geometries.</returns>
         /// <exception cref="ArgumentException">if g is null</exception>
         public static double Distance(this Geometry geometry, Geometry anotherGeometry, bool useSpheroid) => default;
     }
