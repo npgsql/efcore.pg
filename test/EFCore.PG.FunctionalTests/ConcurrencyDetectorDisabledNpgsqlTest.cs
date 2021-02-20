ea9e5d4956d2077d33cabeee0f47ddd56b3c0009
Shay Rojansky <roji@roji.org>
2021-02-20 10:11:53 +0100

    Sync to 6.0.0-preview.2.21119.4 (#1720)
    Commit 7dada2a440e09439a7fc491718ded70f2290f4db

---
+++ test/EFCore.PG.FunctionalTests/ConcurrencyDetectorDisabledNpgsqlTest.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
+using Xunit;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL
+{
+    public class ConcurrencyDetectorDisabledNpgsqlTest : ConcurrencyDetectorDisabledRelationalTestBase<
+        ConcurrencyDetectorDisabledNpgsqlTest.ConcurrencyDetectorNpgsqlFixture>
+    {
+        public ConcurrencyDetectorDisabledNpgsqlTest(ConcurrencyDetectorNpgsqlFixture fixture)
+            : base(fixture)
+        {
+            Fixture.TestSqlLoggerFactory.Clear();
+        }
+
+        public override Task FromSql(bool async)
+            => ConcurrencyDetectorTest(async c => async
+                ? await c.Products.FromSqlRaw(@"select * from ""Products""").ToListAsync()
+                : c.Products.FromSqlRaw(@"select * from ""Products""").ToList());
+
+        protected override async Task ConcurrencyDetectorTest(Func<ConcurrencyDetectorDbContext, Task<object>> test)
+        {
+            await base.ConcurrencyDetectorTest(test);
+
+            Assert.NotEmpty(Fixture.TestSqlLoggerFactory.SqlStatements);
+        }
+
+        public class ConcurrencyDetectorNpgsqlFixture : ConcurrencyDetectorFixtureBase
+        {
+            protected override ITestStoreFactory TestStoreFactory
+                => NpgsqlTestStoreFactory.Instance;
+
+            public TestSqlLoggerFactory TestSqlLoggerFactory
+                => (TestSqlLoggerFactory)ListLoggerFactory;
+
+            public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
+                => builder.DisableConcurrencyDetection();
+        }
+    }
+}
