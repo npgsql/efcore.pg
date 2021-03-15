04065e07d544078e99db4e4b2efbec9c6df37bdc
Shay Rojansky <roji@roji.org>
2021-03-15 13:34:44 +0100

    Bump dependencies (#1762)
    Bumps `EFCoreVersion` from 6.0.0-preview.3.21158.1 to 6.0.0-preview.3.21163.3.
    Bumps `MicrosoftExtensionsVersion` from 6.0.0-preview.3.21157.6 to 6.0.0-preview.3.21164.8.
    Bumps `Npgsql` from 6.0.0-ci.20210303T093005+sha.cf73c6e0b to 6.0.0-ci.20210310T201503.

---
+++ test/EFCore.PG.FunctionalTests/DesignTimeNpgsqlTest.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Design.Internal;
+using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL
+{
+    public class DesignTimeNpgsqlTest : DesignTimeTestBase<DesignTimeNpgsqlTest.DesignTimeNpgsqlFixture>
+    {
+        public DesignTimeNpgsqlTest(DesignTimeNpgsqlFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        protected override Assembly ProviderAssembly
+            => typeof(NpgsqlDesignTimeServices).Assembly;
+
+        public class DesignTimeNpgsqlFixture : DesignTimeFixtureBase
+        {
+            protected override ITestStoreFactory TestStoreFactory
+                => NpgsqlTestStoreFactory.Instance;
+        }
+    }
+}
--- test/EFCore.PG.FunctionalTests/Query/ComplexNavigationsSharedTypeQueryNpgsqlTest.cs
+++ test/EFCore.PG.FunctionalTests/Query/ComplexNavigationsSharedTypeQueryNpgsqlTest.cs
@@ -24,14 +24,6 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query
             // Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
         }
 
-        public override async Task SelectMany_with_navigation_and_Distinct(bool async)
-        {
-            var message = (await Assert.ThrowsAsync<InvalidOperationException>(
-                () => base.SelectMany_with_navigation_and_Distinct(async))).Message;
-
-            Assert.Equal(RelationalStrings.InsufficientInformationToIdentifyOuterElementOfCollectionJoin, message);
-        }
-
         [ConditionalTheory(Skip = "https://github.com/dotnet/efcore/pull/22532")]
         public override Task Distinct_skip_without_orderby(bool async)
             => base.Distinct_skip_without_orderby(async);
