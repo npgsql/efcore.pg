daf15452a4715cc5053ed477c3570db722f617a6
Shay Rojansky <roji@roji.org>
2021-03-23 16:40:59 +0100

    Fixes to precision/scale handling in type mappings (#1778)
    * Fix store type name generation for timestamp
    * Refactor store type name generation
    * ~Properly flow facets to array element mappings~
    
    Fixes #1775
    
    (cherry picked from commit 4a755260f73f210f3d29e831faa6f16fcab0a8d6)

---
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqlDecimalTypeMapping.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping
+{
+    public class NpgsqlDecimalTypeMapping : DecimalTypeMapping
+    {
+        public NpgsqlDecimalTypeMapping() : base("numeric", System.Data.DbType.Decimal) {}
+
+        protected NpgsqlDecimalTypeMapping(RelationalTypeMappingParameters parameters)
+            : base(parameters)
+        {
+        }
+
+        protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
+            => new NpgsqlDecimalTypeMapping(parameters);
+
+        protected override string ProcessStoreType(RelationalTypeMappingParameters parameters, string storeType, string _)
+            => parameters.Precision is null
+                ? storeType
+                : parameters.Scale is null
+                    ? $"numeric({parameters.Precision})"
+                    : $"numeric({parameters.Precision},{parameters.Scale})";
+    }
+}
