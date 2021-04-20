70e000d23046dc6a85820b5a8da36d9a84b8b876
Shay Rojansky <roji@roji.org>
2021-04-20 16:53:45 +0300

    Bump dependencies (#1820)
    EFCore -> 6.0.0-preview.5.21219.5 (277b43aab863ee0aea91a0ce09f501e3089a95aa)
    MicrosoftExtensions -> 6.0.0-preview.5.21218.1
    Npgsql -> 6.0.0-ci.20210418T145437

---
+++ test/EFCore.PG.Tests/TestUtilities/FakeProvider/FakeRelationalOptionsExtension.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.EntityFrameworkCore.Update;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities.FakeProvider
+{
+    public class FakeRelationalOptionsExtension : RelationalOptionsExtension
+    {
+        private DbContextOptionsExtensionInfo _info;
+
+        public FakeRelationalOptionsExtension()
+        {
+        }
+
+        protected FakeRelationalOptionsExtension(FakeRelationalOptionsExtension copyFrom)
+            : base(copyFrom)
+        {
+        }
+
+        public override DbContextOptionsExtensionInfo Info
+            => _info ??= new ExtensionInfo(this);
+
+        protected override RelationalOptionsExtension Clone()
+            => new FakeRelationalOptionsExtension(this);
+
+        public override void ApplyServices(IServiceCollection services)
+            => AddEntityFrameworkRelationalDatabase(services);
+
+        public static IServiceCollection AddEntityFrameworkRelationalDatabase(IServiceCollection serviceCollection)
+        {
+            var builder = new EntityFrameworkRelationalServicesBuilder(serviceCollection);
+
+            // Specific test services are available upstream if we need them
+            builder.TryAddCoreServices();
+
+            return serviceCollection;
+        }
+
+        private sealed class ExtensionInfo : RelationalExtensionInfo
+        {
+            public ExtensionInfo(IDbContextOptionsExtension extension)
+                : base(extension)
+            {
+            }
+
+            public override void PopulateDebugInfo(IDictionary<string, string> debugInfo)
+            {
+            }
+        }
+    }
+}
