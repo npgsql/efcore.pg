725a898dd4fe554f249dd33eefdf20cb5fa37db6
Shay Rojansky <roji@roji.org>
2021-05-15 18:11:51 +0200

    Don't generate both IDENTITY and NULL in migrations (#1849)
    Fixes #1848
    
    (cherry picked from commit f248785fdb0d69f792d6d1bdb7af24aeee722aba)

--- src/EFCore.PG/Migrations/NpgsqlMigrationsSqlGenerator.cs
+++ src/EFCore.PG/Migrations/NpgsqlMigrationsSqlGenerator.cs
@@ -1408,12 +1408,16 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Migrations
                     .Append(DelimitIdentifier(collation));
             }
 
-            builder.Append(operation.IsNullable ? " NULL" : " NOT NULL");
-
-            DefaultValue(operation.DefaultValue, operation.DefaultValueSql, columnType, builder);
-
             if (valueGenerationStrategy.IsIdentity())
+            {
                 IdentityDefinition(operation, builder);
+            }
+            else
+            {
+                builder.Append(operation.IsNullable ? " NULL" : " NOT NULL");
+
+                DefaultValue(operation.DefaultValue, operation.DefaultValueSql, columnType, builder);
+            }
         }
 
         // Note: this definition is only used for creating new identity columns, not for alterations.
