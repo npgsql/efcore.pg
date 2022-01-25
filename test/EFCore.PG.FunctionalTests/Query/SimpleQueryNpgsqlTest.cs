14031facffc7c111f9040c5ab240ae02b712643f
Shay Rojansky <roji@roji.org>
2022-01-25 19:38:19 +0100

    Sync dependencies
    EF Core -> 7.0.0-preview.1.22071.1
    Npgsql -> 7.0.0-preview.1-ci.20220123T182030

--- test/EFCore.PG.FunctionalTests/Query/SimpleQueryNpgsqlTest.cs
+++ test/EFCore.PG.FunctionalTests/Query/SimpleQueryNpgsqlTest.cs
@@ -5,4 +5,8 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query;
 public class SimpleQueryNpgsqlTest : SimpleQueryRelationalTestBase
 {
     protected override ITestStoreFactory TestStoreFactory => NpgsqlTestStoreFactory.Instance;
+
+    [ConditionalTheory(Skip = "https://github.com/dotnet/efcore/issues/27278")]
+    public override Task Aggregate_over_subquery_in_group_by_projection(bool async)
+        => base.Aggregate_over_subquery_in_group_by_projection(async);
 }
