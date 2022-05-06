779d8a27d87670f0cc7de6caebd6c3e06580ad0a
Shay Rojansky <roji@roji.org>
2022-05-06 11:11:07 +0200

    Clean up NodaTime evaluatable filter

--- src/EFCore.PG.NodaTime/Query/Internal/NpgsqlNodaTimeEvaluatableExpressionFilterPlugin.cs
+++ src/EFCore.PG.NodaTime/Query/Internal/NpgsqlNodaTimeEvaluatableExpressionFilterPlugin.cs
@@ -9,11 +9,30 @@ public class NpgsqlNodaTimeEvaluatableExpressionFilterPlugin : IEvaluatableExpre
         typeof(SystemClock).GetMember(nameof(SystemClock.Instance)).FirstOrDefault()!;
 
     public virtual bool IsEvaluatableExpression(Expression expression)
-        => !(
-            expression is MethodCallExpression methodExpression
-            && methodExpression.Method == GetCurrentInstantMethod
-            ||
-            expression is MemberExpression memberExpression
-            && memberExpression.Member == SystemClockInstanceMember
-        );
+    {
+        switch (expression)
+        {
+            case MethodCallExpression methodCallExpression when methodCallExpression.Method == GetCurrentInstantMethod:
+                return false;
+
+            case MemberExpression memberExpression:
+                if (memberExpression.Member == SystemClockInstanceMember)
+                {
+                    return false;
+                }
+
+                // We support translating certain NodaTime patterns which accept a time zone as a parameter,
+                // e.g. Instant.InZone(timezone), as long as the timezone is expressed as an access on DateTimeZoneProviders.Tzdb.
+                // Prevent this from being evaluated locally and so parameterized, so we can access the member access on
+                // DateTimeZoneProviders and extract the constant (see NpgsqlNodaTimeMethodCallTranslatorPlugin)
+                if (memberExpression.Member.DeclaringType == typeof(DateTimeZoneProviders))
+                {
+                    return false;
+                }
+
+                break;
+        }
+
+        return true;
+    }
 }
