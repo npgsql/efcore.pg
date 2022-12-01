09bc34fd07977b86813909d3a61e0ee9049bb6de
Shay Rojansky <roji@roji.org>
2022-12-01 13:06:55 +0100

    Correct date/time mapping with precision (#2580)
    Closes #2579
    
    (cherry picked from commit 9573961411ef9643c10d4cc6a57b03ee8ead62b4)

--- src/EFCore.PG/Storage/Internal/Mapping/NpgsqlTimeTzTypeMapping.cs
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqlTimeTzTypeMapping.cs
@@ -40,6 +40,15 @@ public class NpgsqlTimeTzTypeMapping : NpgsqlTypeMapping
     ///     any release. You should only use it directly in your code with extreme caution and knowing that
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
+    protected override string ProcessStoreType(RelationalTypeMappingParameters parameters, string storeType, string _)
+        => parameters.Precision is null ? storeType : $"time({parameters.Precision}) with time zone";
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
     protected override string GenerateNonNullSqlLiteral(object value)
         => FormattableString.Invariant($"TIMETZ '{(DateTimeOffset)value:HH:mm:ss.FFFFFFz}'");
 
