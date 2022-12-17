e68dfe8b5cbe4a26d20acc36def6187aa1cfdda3
Shay Rojansky <roji@roji.org>
2022-12-17 17:11:59 +0100

    Some test tweaks

--- test/EFCore.PG.FunctionalTests/Query/SimpleQueryNpgsqlTest.cs
+++ test/EFCore.PG.FunctionalTests/Query/SimpleQueryNpgsqlTest.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
+using Xunit;
 
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query;
 
@@ -16,6 +17,10 @@ public class SimpleQueryNpgsqlTest : SimpleQueryRelationalTestBase
     // Writes DateTime with Kind=Unspecified to timestamptz
     public override Task Subquery_first_member_compared_to_null(bool async)
         => Task.CompletedTask;
+
+    [ConditionalTheory(Skip = "https://github.com/dotnet/efcore/pull/27995/files#r874038747")]
+    public override Task StoreType_for_UDF_used(bool async)
+        => base.StoreType_for_UDF_used(async);
 
     // https://github.com/dotnet/efcore/pull/27280, fixed for 7.0
     public override Task Aggregate_over_subquery_in_group_by_projection(bool async)
