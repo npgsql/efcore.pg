e5ade7f0685c3d850083a999f186c25004ef82b2
Shay Rojansky <roji@roji.org>
2023-03-15 13:25:37 +0100

    Allow mapping decimal to float/double (#2693)
    Fixes #2692
    
    (cherry picked from commit 5ae141aefb210d28995cc4f29ec7faf84eccf117)

--- src/EFCore.PG/Storage/Internal/Mapping/NpgsqlDecimalTypeMapping.cs
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqlDecimalTypeMapping.cs
@@ -1,13 +1,16 @@
+using System;
 using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping;
 
-public class NpgsqlDecimalTypeMapping : DecimalTypeMapping
+public class NpgsqlDecimalTypeMapping : NpgsqlTypeMapping
 {
-    public NpgsqlDecimalTypeMapping() : base("numeric", System.Data.DbType.Decimal) {}
+    private const string DecimalFormatConst = "{0:0.0###########################}";
+
+    public NpgsqlDecimalTypeMapping(Type? clrType = null) : base("numeric", clrType ?? typeof(decimal), NpgsqlTypes.NpgsqlDbType.Numeric) {}
 
     protected NpgsqlDecimalTypeMapping(RelationalTypeMappingParameters parameters)
-        : base(parameters)
+        : base(parameters, NpgsqlTypes.NpgsqlDbType.Numeric)
     {
     }
 
@@ -20,4 +23,13 @@ public class NpgsqlDecimalTypeMapping : DecimalTypeMapping
             : parameters.Scale is null
                 ? $"numeric({parameters.Precision})"
                 : $"numeric({parameters.Precision},{parameters.Scale})";
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    protected override string SqlLiteralFormatString
+        => DecimalFormatConst;
 }
