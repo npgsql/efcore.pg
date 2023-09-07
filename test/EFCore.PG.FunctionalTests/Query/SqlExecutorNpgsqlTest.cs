737541a3ceafb052ebf0905cd6d974950afcdf01
Giang Pham <giang.phamhoang712@gmail.com>
2023-09-07 21:19:50 +1000

    Skip tests in SqlExecutorNpgsqlTest that tests unsupported features
    Github issue: https://github.com/cockroachdb/cockroach/issues/100226

--- test/EFCore.PG.FunctionalTests/Query/SqlExecutorNpgsqlTest.cs
+++ test/EFCore.PG.FunctionalTests/Query/SqlExecutorNpgsqlTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Common;
+using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
 
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query;
 
@@ -21,4 +22,22 @@ public class SqlExecutorNpgsqlTest : SqlExecutorTestBase<NorthwindQueryNpgsqlFix
     protected override string CustomerOrderHistorySproc => @"SELECT * FROM ""CustOrderHist""(@CustomerID)";
 
     protected override string CustomerOrderHistoryWithGeneratedParameterSproc => @"SELECT * FROM ""CustOrderHist""({0})";
+
+    [SkipForCockroachDb("CockroachDB doesn't support function that returns TABLE, https://github.com/cockroachdb/cockroach/issues/100226")]
+    [ConditionalTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public override Task Executes_stored_procedure_with_generated_parameter(bool async)
+    {
+        return base.Executes_stored_procedure_with_generated_parameter(async);
+    }
+
+    [SkipForCockroachDb("CockroachDB doesn't support function that returns TABLE, https://github.com/cockroachdb/cockroach/issues/100226")]
+    [ConditionalTheory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public override Task Executes_stored_procedure_with_parameter(bool async)
+    {
+        return base.Executes_stored_procedure_with_parameter(async);
+    }
 }
