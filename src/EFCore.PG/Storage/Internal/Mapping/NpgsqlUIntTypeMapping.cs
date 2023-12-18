baaf41f4b6a3bba4c705670d09b76f793111bbe3
Shay Rojansky <roji@roji.org>
2023-12-18 13:47:28 +0200

    Support NpgsqlUIntTypeMapping for the compiled model (#3030)
    Fixes #2992
    
    (cherry picked from commit 74790424d7c954c43fd9848aa635bb5bd091cbe3)

--- src/EFCore.PG/Storage/Internal/Mapping/NpgsqlUIntTypeMapping.cs
+++ src/EFCore.PG/Storage/Internal/Mapping/NpgsqlUIntTypeMapping.cs
@@ -16,6 +16,14 @@ public class NpgsqlUIntTypeMapping : NpgsqlTypeMapping
     ///     any release. You should only use it directly in your code with extreme caution and knowing that
     ///     doing so can result in application failures when updating to a new Entity Framework Core release.
     /// </summary>
+    public static NpgsqlUIntTypeMapping Default { get; } = new("xid", NpgsqlDbType.Xid);
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
     public NpgsqlUIntTypeMapping(string storeType, NpgsqlDbType npgsqlDbType)
         : base(storeType, typeof(uint), npgsqlDbType, JsonUInt32ReaderWriter.Instance)
     {
