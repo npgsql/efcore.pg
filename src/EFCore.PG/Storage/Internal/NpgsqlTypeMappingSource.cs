dba77fd16bf29d825caf5a5c6f78365b43393326
Shay Rojansky <roji@roji.org>
2024-06-28 15:01:28 +0200

    Correct non-NodaTime date/time type mappings when NodaTime is configured (#3214)
    Fixes #3213

--- src/EFCore.PG/Storage/Internal/NpgsqlTypeMappingSource.cs
+++ src/EFCore.PG/Storage/Internal/NpgsqlTypeMappingSource.cs
@@ -11,7 +11,6 @@ using System.Text;
 using System.Text.Json;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.Internal;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping;
-using Npgsql.Internal;
 
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal;
 
