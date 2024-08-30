01e6dee19f58c96288597e740d36c1b11f1bed9c
Christopher Jolly <chris-jolly_au@hotmail.com>
2024-08-30 21:39:29 +0800

    Update summary descriptions

--- src/EFCore.PG/ValueGeneration/Internal/NpgsqlUuid7ValueGenerator.cs
+++ src/EFCore.PG/ValueGeneration/Internal/NpgsqlUuid7ValueGenerator.cs
@@ -4,22 +4,21 @@ using System.Runtime.InteropServices;
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.ValueGeneration.Internal;
 
 /// <summary>
-///     Generates sequential <see cref="Guid" /> values according to the UUID version 7 specification.
-///     Will be updated to use Guid.CreateVersion7 when available.
+///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+///     directly from your code. This API may change or be removed in future releases.
 /// </summary>
 public class NpgsqlUuid7ValueGenerator : ValueGenerator<Guid>
 {
     /// <summary>
-    ///     Gets a value indicating whether the values generated are temporary or permanent. This implementation
-    ///     always returns false, meaning the generated values will be saved to the database.
+    ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
     public override bool GeneratesTemporaryValues => false;
 
     /// <summary>
-    ///     Gets a value to be assigned to a property.
+    ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
-    /// <param name="entry">The change tracking entry of the entity for which the value is being generated.</param>
-    /// <returns>The value to be assigned to a property.</returns>
     public override Guid Next(EntityEntry entry) => BorrowedFromNet9.CreateVersion7(timestamp: DateTimeOffset.UtcNow);
 
     // Code borrowed from .NET 9 should be removed as soon as the target framework includes such code
