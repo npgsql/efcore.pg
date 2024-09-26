2b5d63e15010c53458ba673c9c060a703c6ae657
Shay Rojansky <roji@roji.org>
2024-09-26 22:13:42 +0200

    Test fixes for 8.0.x sync

---
+++ test/EFCore.PG.FunctionalTests/Query/AdHocComplexTypeQueryNpgsqlTest.cs
@@ -0,0 +1,32 @@
+using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
+
+namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query;
+
+public class AdHocComplexTypeQueryNpgsqlTest : AdHocComplexTypeQueryTestBase
+{
+    public override async Task Complex_type_equals_parameter_with_nested_types_with_property_of_same_name()
+    {
+        await base.Complex_type_equals_parameter_with_nested_types_with_property_of_same_name();
+
+        AssertSql(
+            """
+@__entity_equality_container_0_Id='1' (Nullable = true)
+@__entity_equality_container_0_Containee1_Id='2' (Nullable = true)
+@__entity_equality_container_0_Containee2_Id='3' (Nullable = true)
+
+SELECT e."Id", e."ComplexContainer_Id", e."ComplexContainer_Containee1_Id", e."ComplexContainer_Containee2_Id"
+FROM "EntityType" AS e
+WHERE e."ComplexContainer_Id" = @__entity_equality_container_0_Id AND e."ComplexContainer_Containee1_Id" = @__entity_equality_container_0_Containee1_Id AND e."ComplexContainer_Containee2_Id" = @__entity_equality_container_0_Containee2_Id
+LIMIT 2
+""");
+    }
+
+    protected TestSqlLoggerFactory TestSqlLoggerFactory
+        => (TestSqlLoggerFactory)ListLoggerFactory;
+
+    protected void AssertSql(params string[] expected)
+        => TestSqlLoggerFactory.AssertBaseline(expected);
+
+    protected override ITestStoreFactory TestStoreFactory
+        => NpgsqlTestStoreFactory.Instance;
+}
