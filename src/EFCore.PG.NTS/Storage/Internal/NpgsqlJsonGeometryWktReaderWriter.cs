303ffc3bd71910d4bb079dfb9490b6f0f5e5e4fc
Shay Rojansky <roji@roji.org>
2024-09-30 09:55:13 +0200

    Prefix JSON geometry representation with SRID (#3307)
    Fixes #3236

--- src/EFCore.PG.NTS/Storage/Internal/NpgsqlJsonGeometryWktReaderWriter.cs
+++ src/EFCore.PG.NTS/Storage/Internal/NpgsqlJsonGeometryWktReaderWriter.cs
@@ -29,7 +29,18 @@ public sealed class NpgsqlJsonGeometryWktReaderWriter : JsonValueReaderWriter<Ge
 
     /// <inheritdoc />
     public override void ToJsonTyped(Utf8JsonWriter writer, Geometry value)
-        => writer.WriteStringValue(value.ToText());
+    {
+        var wkt = value.ToText();
+
+        // If the SRID is defined, prefix the WKT with it (SRID=4326;POINT(-44.3 60.1))
+        // Although this is a PostgreSQL extension, NetTopologySuite supports it (see #3236)
+        if (value.SRID > 0)
+        {
+            wkt = $"SRID={value.SRID};{wkt}";
+        }
+
+        writer.WriteStringValue(wkt);
+    }
 
     /// <inheritdoc />
     public override Expression ConstructorExpression => Expression.Property(null, InstanceProperty);
