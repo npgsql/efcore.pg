74d8a51eaf4b3848f5eded5ff36dd0b3d87dc218
Shay Rojansky <roji@roji.org>
2024-11-14 14:49:44 +0100

    Use .NET/EF 9.0 GA (#3361)

--- test/EFCore.PG.FunctionalTests/Query/AdHocPrecompiledQueryNpgsqlTest.cs
+++ test/EFCore.PG.FunctionalTests/Query/AdHocPrecompiledQueryNpgsqlTest.cs
@@ -69,6 +69,35 @@ LIMIT 2
 """);
     }
 
+    public override async Task Projecting_property_requiring_converter_with_closure_is_not_supported()
+    {
+        await base.Projecting_property_requiring_converter_with_closure_is_not_supported();
+
+        AssertSql();
+    }
+
+    public override async Task Projecting_expression_requiring_converter_without_closure_works()
+    {
+        await base.Projecting_expression_requiring_converter_without_closure_works();
+
+        AssertSql(
+            """
+SELECT b."AudiobookDate"
+FROM "Books" AS b
+""");
+    }
+
+    public override async Task Projecting_entity_with_property_requiring_converter_with_closure_works()
+    {
+        await base.Projecting_entity_with_property_requiring_converter_with_closure_works();
+
+        AssertSql(
+            """
+SELECT b."Id", b."AudiobookDate", b."Name", b."PublishDate"
+FROM "Books" AS b
+""");
+    }
+
     [ConditionalFact]
     public virtual void Check_all_tests_overridden()
         => TestHelpers.AssertAllMethodsOverridden(GetType());
