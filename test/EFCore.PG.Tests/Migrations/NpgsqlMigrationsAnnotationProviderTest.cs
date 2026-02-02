9a62ae07d6b5eafd29123639d73511045a0c7867
Brian Pursley <bpursley@cinlogic.com>
2026-02-01 20:37:25 -0500

    Update unit test to use extension methods to create annotations.

--- test/EFCore.PG.Tests/Migrations/NpgsqlMigrationsAnnotationProviderTest.cs
+++ test/EFCore.PG.Tests/Migrations/NpgsqlMigrationsAnnotationProviderTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.Internal;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Migrations.Internal;
 
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Migrations;
@@ -11,26 +12,33 @@ public class NpgsqlMigrationsAnnotationProviderTest
     public virtual void ForRemove_returns_relational_model_annotations()
     {
         var modelBuilder = new ModelBuilder()
-            .HasAnnotation("Npgsql:PostgresExtension:Foo1", "SomeValue")
-            .HasAnnotation("Npgsql:Enum:Foo2", "SomeValue")
-            .HasAnnotation("Npgsql:Range:Foo3", "SomeValue")
-            .HasAnnotation("Npgsql:CollationDefinition:Foo4", "SomeValue")
-            .HasAnnotation("Npgsql:SomethingElse:Foo5", "SomeValue") // Should be ignored by ForRemove()
-            .HasAnnotation("Npgsql:Another", "SomeValue") // Should be ignored by ForRemove()
-            .HasAnnotation("PostgresExtension:Foo1", "SomeValue") // Should be ignored by ForRemove()
-            .HasAnnotation("Enum:Foo2", "SomeValue") // Should be ignored by ForRemove()
-            .HasAnnotation("Range:Foo3", "SomeValue") // Should be ignored by ForRemove()
-            .HasAnnotation("CollationDefinition:Foo4", "SomeValue"); // Should be ignored by ForRemove()
+            .HasPostgresExtension("test_extension")
+            .HasPostgresExtension("test_schema2", "test_extension2")
+            .HasPostgresEnum("test_enum", ["A", "B", "C"])
+            .HasPostgresEnum("test_schema2", "test_enum2", ["A", "B", "C"])
+            .HasPostgresRange("test_range", "test_subtype")
+            .HasPostgresRange("test_schema2", "test_range2", "test_subtype2")
+            .HasCollation("test_collation", "test_locale")
+            .HasCollation("test_schema2", "test_collation2", "test_locale2", "test_provider2", false);
+
+        // Define a sequence, so we can verify that the annotation it creates is excluded from the ForRemove() result.
+        modelBuilder.HasSequence("test_sequence");
 
         var model = new RelationalModel(modelBuilder.FinalizeModel());
+        var allAnnotations = model.Model.GetAnnotations().ToList();
 
         var annotations = _migrationsAnnotationProvider.ForRemove(model).ToList();
 
-        Assert.Equal(4, annotations.Count);
-        Assert.Contains(annotations, a => a.Name == "Npgsql:PostgresExtension:Foo1");
-        Assert.Contains(annotations, a => a.Name == "Npgsql:Enum:Foo2");
-        Assert.Contains(annotations, a => a.Name == "Npgsql:Range:Foo3");
-        Assert.Contains(annotations, a => a.Name == "Npgsql:CollationDefinition:Foo4");
+        Assert.Equal(9, allAnnotations.Count);
+        Assert.Equal(8, annotations.Count);
+        Assert.Contains(annotations, a => a.Name == $"{NpgsqlAnnotationNames.PostgresExtensionPrefix}test_extension");
+        Assert.Contains(annotations, a => a.Name == $"{NpgsqlAnnotationNames.PostgresExtensionPrefix}test_schema2.test_extension2");
+        Assert.Contains(annotations, a => a.Name == $"{NpgsqlAnnotationNames.EnumPrefix}test_enum");
+        Assert.Contains(annotations, a => a.Name == $"{NpgsqlAnnotationNames.EnumPrefix}test_schema2.test_enum2");
+        Assert.Contains(annotations, a => a.Name == $"{NpgsqlAnnotationNames.RangePrefix}test_range");
+        Assert.Contains(annotations, a => a.Name == $"{NpgsqlAnnotationNames.RangePrefix}test_schema2.test_range2");
+        Assert.Contains(annotations, a => a.Name == $"{NpgsqlAnnotationNames.CollationDefinitionPrefix}test_collation");
+        Assert.Contains(annotations, a => a.Name == $"{NpgsqlAnnotationNames.CollationDefinitionPrefix}test_schema2.test_collation2");
     }
 
     [Fact]
